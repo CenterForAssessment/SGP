7398c9145e019015100c292ef0f754be9a824a8a
Adam VanIwaarden <avaniwaarden@gmail.com>
2012-03-27 09:17:24 -0500

    0.9-0.0 outputSGP: major revisions
    Add output of SUMMARY, INDIVIDUAL results, output type ("SchoolView")

--- R/outputSGP.R
+++ R/outputSGP.R
@@ -1,16 +1,24 @@
 `outputSGP` <- 
 function(sgp_object,
-	state,
-	output.type="SchoolView_WIDE",
+	state=NULL,
+	output.type="SchoolView",
 	baseline.sgps=FALSE,
-	outputSGP.years=NULL,
-	outputSGP.content_areas=NULL,
-	outputSGP.anonymize=FALSE) {
+	outputSGP_SUMMARY.years=NULL,
+	outputSGP_SUMMARY.content_areas=NULL,
+	outputSGP_INDIVIDUAL.years=NULL,
+	outputSGP_INDIVIDUAL.content_areas=NULL,
+	outputSGP.anonymize=FALSE,
+	outputSGP.student.groups=NULL,
+	outputSGP.directory=file.path("Data", "SchoolView")) {
 
+	### Define varaibles (to prevent R CMD check warnings)
+
+	SCALE_SCORE <- CONTENT_AREA <- YEAR <- GRADE <- ID <- ETHNICITY <- GENDER <- LAST_NAME <- FIRST_NAME <- VALID_CASE <- DISTRICT_NUMBER <- SCHOOL_NUMBER <- NULL
+	names.type <- names.provided <- names.output <- NULL
 
 	### Create state (if missing) from sgp_object (if possible)
 
-        if (missing(state)) {
+        if (is.null(state)) {
                 tmp.name <- gsub("_", " ", deparse(substitute(sgp_object)))
                 if (any(sapply(c(state.name, "Demonstration"), function(x) regexpr(x, tmp.name)))==1) {
                         state <- c(state.abb, "DEMO")[which(sapply(c(state.name, "Demonstration"), function(x) regexpr(x, tmp.name))==1)]
@@ -18,12 +26,31 @@ function(sgp_object,
         }
 
 
-if ("SchoolView_WIDE" %in% output.type) {
+	### Create relevant variables
 
-	### Define varaibles (to prevent R CMD check warnings)
+	if (is.null(outputSGP.student.groups)) {
+		outputSGP.student.groups <- intersect(names(sgp_object@Data), subset(sgp_object@Names, names.type=="demographic" & names.output==TRUE, select=names.provided, drop=TRUE))
+	}
 
-	SCALE_SCORE <- CONTENT_AREA <- YEAR <- GRADE <- ID <- ETHNICITY <- GENDER <- LAST_NAME <- FIRST_NAME <- VALID_CASE <- DISTRICT_NUMBER <- SCHOOL_NUMBER <- NULL
 
+if ("SchoolView" %in% output.type) {
+
+	### 
+	### Summary Tables
+	###
+
+	sqliteSGP(
+		sgp_object=sgp_object,
+		state=state,
+		years=outputSGP_SUMMARY.years,
+		content_areas=outputSGP_SUMMARY.content_areas,
+		other.student.groups=outputSGP.student.groups,
+		output.directory=outputSGP.directory)
+	
+
+	###
+	### WIDE Data
+	###
 
 	### Utility functions
 
@@ -84,22 +111,22 @@ if ("SchoolView_WIDE" %in% output.type) {
 
 	#### Year stuff 
 
-	if (is.null(outputSGP.years)) {
+	if (is.null(outputSGP_INDIVIDUAL.years)) {
 		tmp.years <- sort(unique(sgp_object@Data[J("VALID_CASE")][["YEAR"]]))
 		tmp.last.year <- tail(tmp.years, 1)
 	} else {
 		tmp.all.years <- sort(unique(sgp_object@Data[J("VALID_CASE")][["YEAR"]])) 
-		tmp.years <- tmp.all.years[1:which(tmp.all.years==tail(sort(outputSGP.years), 1))]
+		tmp.years <- tmp.all.years[1:which(tmp.all.years==tail(sort(outputSGP_INDIVIDUAL.years), 1))]
 		tmp.last.year <- tail(tmp.years, 1)
 	}
 
 
 	#### Content area stuff 
 
-	if (is.null(outputSGP.content_areas)) {
+	if (is.null(outputSGP_INDIVIDUAL.content_areas)) {
 		tmp.content_areas <- sort(unique(sgp_object@Data[J("VALID_CASE", tmp.last.year)][["CONTENT_AREA"]]))
 	} else {
-		tmp.content_areas <- as.factor(outputSGP.content_areas)
+		tmp.content_areas <- as.factor(outputSGP_INDIVIDUAL.content_areas)
 	}
 
 
@@ -113,7 +140,7 @@ if ("SchoolView_WIDE" %in% output.type) {
         tmp.table <- sgp_object@Data[CJ(report.ids, tmp.content_areas, tmp.years)]
 
 
-	### Create transformed scale scores (NOT necessary if wide data is provided)
+	### Create transformed scale scores
 
 	setkeyv(tmp.table, c("CONTENT_AREA", "YEAR", "GRADE"))
 	tmp.table$TRANSFORMED_SCALE_SCORE <- tmp.table[,
@@ -123,7 +150,7 @@ if ("SchoolView_WIDE" %in% output.type) {
 
 	#### Anonymize (if requested) (NOT necessary if wide data is provided)
      
-	if (outputSGP.anonymize) {
+	if (outputSGP.anonymize | !all(c("LAST_NAME", "FIRST_NAME") %in% names(tmp.table))) {
 		require(randomNames)
                 if (!"ETHNICITY" %in% names(tmp.table)) tmp.table[["ETHNICITY"]] <- 1
                 if (!"GENDER" %in% names(tmp.table)) tmp.table[["GENDER"]] <- round(runif(dim(tmp.table)[1], min=0, max=1))
@@ -146,7 +173,7 @@ if ("SchoolView_WIDE" %in% output.type) {
 
 	variables.to.keep <- c("VALID_CASE", "ID", "LAST_NAME", "FIRST_NAME", "CONTENT_AREA", "YEAR", "GRADE", "EMH_LEVEL", 
 		"SCALE_SCORE", "TRANSFORMED_SCALE_SCORE", "ACHIEVEMENT_LEVEL", "SGP", "SGP_TARGET", "SCHOOL_NUMBER", "DISTRICT_NUMBER",
-		"GENDER", "ETHNICITY", "FREE_REDUCED_LUNCH_STATUS", "ELL_STATUS", "IEP_STATUS", "GIFTED_AND_TALENTED_PROGRAM_STATUS",
+		outputSGP.student.groups,
 		"SCHOOL_ENROLLMENT_STATUS", "DISTRICT_ENROLLMENT_STATUS", "STATE_ENROLLMENT_STATUS")
 
 	outputSGP.data <- reshape(tmp.table[VALID_CASE=="VALID_CASE", variables.to.keep, with=FALSE],
@@ -188,31 +215,28 @@ if ("SchoolView_WIDE" %in% output.type) {
 	
 	## Rename variables to keep
 
-	names(outputSGP.data)[which(names(outputSGP.data)=="ID")] <- "STATE_ASSIGNED_ID"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("LAST_NAME", tmp.last.year, sep="."))] <- "LAST_NAME"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("FIRST_NAME", tmp.last.year, sep="."))] <- "FIRST_NAME"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("DISTRICT_NUMBER", tmp.last.year, sep="."))] <- "DISTRICT_NUMBER"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("SCHOOL_NUMBER", tmp.last.year, sep="."))] <- "SCHOOL_NUMBER"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("EMH_LEVEL", tmp.last.year, sep="."))] <- "EMH_LEVEL"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("ETHNICITY", tmp.last.year, sep="."))] <- "ETHNICITY"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("GENDER", tmp.last.year, sep="."))] <- "GENDER"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("IEP_STATUS", tmp.last.year, sep="."))] <- "IEP_CODE"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("FREE_REDUCED_LUNCH_STATUS", tmp.last.year, sep="."))] <- "FRL_CODE"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("GIFTED_AND_TALENTED_PROGRAM_STATUS", tmp.last.year, sep="."))] <- "GIFTED_CODE"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("ELL_STATUS", tmp.last.year, sep="."))] <- "LANGUAGE_PROFICIENCY"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("SCHOOL_ENROLLMENT_STATUS", tmp.last.year, sep="."))] <- "SCHOOL_ENROLLMENT_STATUS"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("DISTRICT_ENROLLMENT_STATUS", tmp.last.year, sep="."))] <- "DISTRICT_ENROLLMENT_STATUS"
-	names(outputSGP.data)[which(names(outputSGP.data)==paste("STATE_ENROLLMENT_STATUS", tmp.last.year, sep="."))] <- "STATE_ENROLLMENT_STATUS"
+	setnames(outputSGP.data, which(names(outputSGP.data)=="ID"), "STATE_ASSIGNED_ID")
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("LAST_NAME", tmp.last.year, sep=".")), "LAST_NAME")
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("FIRST_NAME", tmp.last.year, sep=".")), "FIRST_NAME")
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("DISTRICT_NUMBER", tmp.last.year, sep=".")), "DISTRICT_NUMBER")
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("SCHOOL_NUMBER", tmp.last.year, sep=".")), "SCHOOL_NUMBER")
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("EMH_LEVEL", tmp.last.year, sep=".")), "EMH_LEVEL")
+	for (i in outputSGP.student.groups) {
+		setnames(outputSGP.data, which(names(outputSGP.data)==paste(i, tmp.last.year, sep=".")), i)
+	}
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("SCHOOL_ENROLLMENT_STATUS", tmp.last.year, sep=".")), "SCHOOL_ENROLLMENT_STATUS")
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("DISTRICT_ENROLLMENT_STATUS", tmp.last.year, sep=".")), "DISTRICT_ENROLLMENT_STATUS")
+	setnames(outputSGP.data, which(names(outputSGP.data)==paste("STATE_ENROLLMENT_STATUS", tmp.last.year, sep=".")), "STATE_ENROLLMENT_STATUS")
 
 	for (i in seq_along(tmp.years)) {	
-		names(outputSGP.data)[grep(paste("GRADE", rev(tmp.years)[i], sep="."), names(outputSGP.data))] <- paste("GRADE_LEVEL", tmp.order[i], sep="_")
-		names(outputSGP.data)[grep(paste("SCALE_SCORE", rev(tmp.years)[i], sep="."), names(outputSGP.data)) %w/o% 
-			grep(paste("TRANSFORMED_SCALE_SCORE", rev(tmp.years)[i], sep="."), names(outputSGP.data))] <- 
-			paste("SCALE_SCORE", tmp.order[i], sep="_")
-		names(outputSGP.data)[grep(paste("TRANSFORMED_SCALE_SCORE", rev(tmp.years)[i], sep="."), names(outputSGP.data))] <- paste("TRANSFORMED_SCALE_SCORE", tmp.order[i], sep="_")
-		names(outputSGP.data)[grep(paste("SGP_TARGET", rev(tmp.years)[i], sep="."), names(outputSGP.data))] <- paste("GROWTH_TARGET", tmp.order[i], sep="_")
-		names(outputSGP.data)[grep(paste("SGP", rev(tmp.years)[i], sep="."), names(outputSGP.data))] <- paste("GROWTH_PERCENTILE", tmp.order[i], sep="_")
-		names(outputSGP.data)[grep(paste("ACHIEVEMENT_LEVEL", rev(tmp.years)[i], sep="."), names(outputSGP.data))] <- paste("PERFORMANCE_LEVEL", tmp.order[i], sep="_")
+		setnames(outputSGP.data, grep(paste("GRADE", rev(tmp.years)[i], sep="."), names(outputSGP.data)), paste("GRADE_LEVEL", tmp.order[i], sep="_"))
+		setnames(outputSGP.data, grep(paste("SCALE_SCORE", rev(tmp.years)[i], sep="."), names(outputSGP.data)) %w/o% 
+			grep(paste("TRANSFORMED_SCALE_SCORE", rev(tmp.years)[i], sep="."), names(outputSGP.data)), 
+			paste("SCALE_SCORE", tmp.order[i], sep="_"))
+		setnames(outputSGP.data, grep(paste("TRANSFORMED_SCALE_SCORE", rev(tmp.years)[i], sep="."), names(outputSGP.data)), paste("TRANSFORMED_SCALE_SCORE", tmp.order[i], sep="_"))
+		setnames(outputSGP.data, grep(paste("SGP_TARGET", rev(tmp.years)[i], sep="."), names(outputSGP.data)), paste("GROWTH_TARGET", tmp.order[i], sep="_"))
+		setnames(outputSGP.data, grep(paste("SGP", rev(tmp.years)[i], sep="."), names(outputSGP.data)), paste("GROWTH_PERCENTILE", tmp.order[i], sep="_"))
+		setnames(outputSGP.data, grep(paste("ACHIEVEMENT_LEVEL", rev(tmp.years)[i], sep="."), names(outputSGP.data)), paste("PERFORMANCE_LEVEL", tmp.order[i], sep="_"))
 	}
 	
 	## NULLify variable to be removed
@@ -223,12 +247,9 @@ if ("SchoolView_WIDE" %in% output.type) {
 		outputSGP.data[[paste("DISTRICT_NUMBER", i, sep=".")]] <- NULL
 		outputSGP.data[[paste("SCHOOL_NUMBER", i, sep=".")]] <- NULL
 		outputSGP.data[[paste("EMH_LEVEL", i, sep=".")]] <- NULL
-		outputSGP.data[[paste("ETHNICITY", i, sep=".")]] <- NULL
-		outputSGP.data[[paste("GENDER", i, sep=".")]] <- NULL
-		outputSGP.data[[paste("IEP_STATUS", i, sep=".")]] <- NULL
-		outputSGP.data[[paste("FREE_REDUCED_LUNCH_STATUS", i, sep=".")]] <- NULL
-		outputSGP.data[[paste("GIFTED_AND_TALENTED_PROGRAM_STATUS", i, sep=".")]] <- NULL
-		outputSGP.data[[paste("ELL_STATUS", i, sep=".")]] <- NULL
+		for (j in outputSGP.student.groups) {
+			outputSGP.data[[paste(j, i, sep=".")]] <- NULL
+		}
 		outputSGP.data[[paste("SCHOOL_ENROLLMENT_STATUS", i, sep=".")]] <- NULL
 		outputSGP.data[[paste("DISTRICT_ENROLLMENT_STATUS", i, sep=".")]] <- NULL
 		outputSGP.data[[paste("STATE_ENROLLMENT_STATUS", i, sep=".")]] <- NULL
@@ -256,7 +277,7 @@ if ("SchoolView_WIDE" %in% output.type) {
 
 	if (length(names(outputSGP.data)[grep("YEAR_1", names(outputSGP.data))]) == 4) {
 		for (i in 1:3) {
-			names(outputSGP.data)[grep(paste("YEAR", i, sep="_"), names(outputSGP.data))] <- paste("CUT", c(1, 35, 65, 99), "YEAR", i, sep="_")
+			setnames(outputSGP.data, grep(paste("YEAR", i, sep="_"), names(outputSGP.data)), paste("CUT", c(1, 35, 65, 99), "YEAR", i, sep="_"))
 			for (j in c(20, 40, 60, 80)) {
 				outputSGP.data[[paste("CUT", j, "YEAR", i, sep="_")]] <- NA	
 			}
@@ -264,9 +285,9 @@ if ("SchoolView_WIDE" %in% output.type) {
 	}
 	if (length(names(outputSGP.data)[grep("YEAR_1", names(outputSGP.data))]) == 6) {
 		for (i in 1:3) {
-			names(outputSGP.data)[grep(paste("YEAR", i, sep="_"), names(outputSGP.data))] <- paste("CUT", c(1, 20, 40, 60, 80, 99), "YEAR", i, sep="_")
+			setnames(outputSGP.data, grep(paste("YEAR", i, sep="_"), names(outputSGP.data)), paste("CUT", c(1, 20, 40, 60, 80, 99), "YEAR", i, sep="_"))
 			for (j in c(35, 65)) {
-				outputSGP.data[[paste("CUT", j, "YEAR", i, sep="_")]] <- NA	
+				outputSGP.data[[paste("CUT", j, "YEAR", i, sep="_")]] <- NA
 			}
 		}
 	}
@@ -274,8 +295,8 @@ if ("SchoolView_WIDE" %in% output.type) {
 	## Rearrange variables
 
 	tmp.variable.names <- c("STUDENT_GROWTH_ID", "STATE_ASSIGNED_ID", "LAST_NAME", "FIRST_NAME", "MIDDLE_NAME", 
-		"CONTENT_AREA", "YEAR", "DISTRICT_NUMBER", "SCHOOL_NUMBER", "EMH_LEVEL", 
-		"ETHNICITY", "GENDER", "IEP_CODE", "FRL_CODE", "HLS_CODE", "GIFTED_CODE", "LANGUAGE_PROFICIENCY",
+		"CONTENT_AREA", "YEAR", "DISTRICT_NUMBER", "SCHOOL_NUMBER", "EMH_LEVEL",
+		outputSGP.student.groups,
 		"OCTOBER_ENROLLMENT_STATUS", "SCHOOL_ENROLLMENT_STATUS", "DISTRICT_ENROLLMENT_STATUS", "STATE_ENROLLMENT_STATUS",
 		"GRADE_LEVEL_CY", "SCALE_SCORE_CY", "TRANSFORMED_SCALE_SCORE_CY", "PERFORMANCE_LEVEL_CY", "GROWTH_PERCENTILE_CY", "GROWTH_TARGET_CY",
 		"GRADE_LEVEL_PY1", "SCALE_SCORE_PY1", "TRANSFORMED_SCALE_SCORE_PY1", "PERFORMANCE_LEVEL_PY1", "GROWTH_PERCENTILE_PY1", "GROWTH_TARGET_PY1",
@@ -289,9 +310,10 @@ if ("SchoolView_WIDE" %in% output.type) {
 		"CUT_1_YEAR_2", "CUT_99_YEAR_2", "CUT_35_YEAR_2", "CUT_65_YEAR_2", "CUT_20_YEAR_2", "CUT_40_YEAR_2", "CUT_60_YEAR_2", "CUT_80_YEAR_2",
 		"CUT_1_YEAR_3", "CUT_99_YEAR_3", "CUT_35_YEAR_3", "CUT_65_YEAR_3", "CUT_20_YEAR_3", "CUT_40_YEAR_3", "CUT_60_YEAR_3", "CUT_80_YEAR_3")
 
+	write.table(outputSGP.data[,tmp.variable.names, with=FALSE], file=file.path(outputSGP.directory, "SchoolView_WIDE.dat"), row.names=FALSE, na="", quote=FALSE, sep="|")
 	outputSGP.data[,tmp.variable.names, with=FALSE]
 
-} ## End if SchoolView_WIDE %in% output.type
+} ## End if SchoolView %in% output.type
 
 
 
