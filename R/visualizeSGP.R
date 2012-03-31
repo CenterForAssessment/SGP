7b771ac1ac5fb2b8fb3886f4695cf9a5d4c30c30
Adam VanIwaarden <avaniwaarden@gmail.com>
2012-03-31 08:44:25 -0500

    vizSGP end of input error again...

--- R/visualizeSGP.R
+++ R/visualizeSGP.R
@@ -214,7 +214,8 @@ function(sgp_object,
 		} ## END FOREACH
 		if(par.start$par.type=="SNOW") {
 			gaPlot.list <- get.gaPlot.iter(gaPlot.years, gaPlot.content_areas, gaPlot.students)
-			parLapply(par.start$internal.cl, gaPlot.list, function(gaPlot.iter) growthAchievementPlot(
+			parLapply(par.start$internal.cl, gaPlot.list, function(gaPlot.iter) 
+				growthAchievementPlot(
 						gaPlot.sgp_object=sgp_object,
 						gaPlot.students=gaPlot.iter[["ID"]],
 						gaPlot.max.order.for.progression=get.max.order.for.progression(gaPlot.iter[["YEAR"]], gaPlot.iter[["CONTENT_AREA"]]),
@@ -227,7 +228,8 @@ function(sgp_object,
 		}
 		if(par.start$par.type=="MULTICORE") {
 			gaPlot.list <- get.gaPlot.iter(gaPlot.years, gaPlot.content_areas, gaPlot.students)
-			mclapply(gaPlot.list, function(gaPlot.iter) growthAchievementPlot(
+			mclapply(gaPlot.list, function(gaPlot.iter) 
+				growthAchievementPlot(
 						gaPlot.sgp_object=sgp_object,
 						gaPlot.students=gaPlot.iter[["ID"]],
 						gaPlot.max.order.for.progression=get.max.order.for.progression(gaPlot.iter[["YEAR"]], gaPlot.iter[["CONTENT_AREA"]]),
@@ -731,8 +733,10 @@ if (sgPlot.produce.plots) {
 		} ### END if FOREACH
 
 		if(par.start$par.type=="SNOW") {
+
 			sgPlot.list <- get.sgPlot.iter(tmp.districts.and.schools)
-			parLapply(par.start$internal.cl, sgPlot.list, function(sgPlot.iter) invisible(studentGrowthPlot_Styles(
+			parLapply(par.start$internal.cl, sgPlot.list, function(sgPlot.iter) 
+				invisible(studentGrowthPlot_Styles(
 					sgPlot.data=sgPlot.data,
 					state=state,
 					last.year=tmp.last.year,
@@ -753,8 +757,10 @@ if (sgPlot.produce.plots) {
 		} ### END if SNOW
 
 		if(par.start$par.type=="MULTICORE") {
+
 			sgPlot.list <- get.sgPlot.iter(tmp.districts.and.schools)
-			mclapply(sgPlot.list, function(sgPlot.iter) invisible(studentGrowthPlot_Styles(
+			mclapply(sgPlot.list, function(sgPlot.iter) 
+				invisible(studentGrowthPlot_Styles(
 					sgPlot.data=sgPlot.data,
 					state=state,
 					last.year=tmp.last.year,
