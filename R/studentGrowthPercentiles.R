95af1e119316041dacd95d422caa8330887012d1
Adam VanIwaarden <avaniwaarden@gmail.com>
2012-08-06 21:39:03 -0600

    Change matrix naming for grade.progression.label - only grades use in progression (not full progression)

--- R/studentGrowthPercentiles.R
+++ R/studentGrowthPercentiles.R
@@ -227,7 +227,7 @@ function(panel.data,         ## REQUIRED
 
 	get.coefficient.matrix.label <- function(tmp.last, k, grade.progression.label) {
 		if (grade.progression.label) {
-			return(paste("qrmatrix_", tmp.last, "_", k, "_", paste(tmp.gp, collapse="."), sep=""))
+			return(paste("qrmatrix_", tmp.last, "_", k, "_", paste(c(tail(tmp.gp[1:num.prior], k), tmp.last), collapse="."), sep=""))
 		} else {
 			return(paste("qrmatrix_", tmp.last, "_", k, sep=""))
 		}
@@ -237,7 +237,8 @@ function(panel.data,         ## REQUIRED
 		.check.my.coefficient.matrices(matrix.names, tmp.last, order)
 		mod <- character()
 		if (grade.progression.label) {
-			tmp.mtx <- eval(parse(text=paste("Coefficient_Matrices[['", tmp.path.coefficient.matrices, "']][['qrmatrix_", tmp.last, "_", j, "_", paste(c(tail(tmp.gp[1:num.prior], order), tmp.last), collapse="."), "']]", sep="")))
+			tmp.mtx <- eval(parse(text=paste("Coefficient_Matrices[['", tmp.path.coefficient.matrices, "']][['qrmatrix_", tmp.last, "_", j, "_", 
+				paste(c(tail(tmp.gp[1:num.prior], order), tmp.last), collapse="."), "']]", sep="")))
 		} else {
 			tmp.mtx <- eval(parse(text=paste("Coefficient_Matrices[['", tmp.path.coefficient.matrices, "']][['qrmatrix_", tmp.last, "_", j, "']]", sep="")))
 		}
