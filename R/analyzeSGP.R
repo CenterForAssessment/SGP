61a45c25fc065de8d7dbeebb938f0776eaa27a55
Damian Betebenner <dbetebenner@nciea.org>
2012-08-30 14:50:20 -0400

    Bug fix in get.knots.boundaries for edge case with different content area priors

--- R/analyzeSGP.R
+++ R/analyzeSGP.R
@@ -360,7 +360,7 @@ function(sgp_object,
 
 		get.my.knots.boundaries.path <- function(content_area, year) {
 			tmp.knots.boundaries.names <- names(SGPstateData[[state]][['Achievement']][['Knots_Boundaries']])
-			tmp.knots.boundaries.names <- tmp.knots.boundaries.names[content_area==tmp.knots.boundaries.names]
+			tmp.knots.boundaries.names <- tmp.knots.boundaries.names[grep(content_area, tmp.knots.boundaries.names)]
 			tmp.knots.boundaries.years <- sapply(strsplit(tmp.knots.boundaries.names, "[.]"), function(x) x[2])
 			if (any(!is.na(tmp.knots.boundaries.years))) {
 				if (year %in% tmp.knots.boundaries.years) {
@@ -407,7 +407,8 @@ function(sgp_object,
 			}
 		}
 		return(kb[["Knots_Boundaries"]])
-	}
+
+	} ### END get.knots.boundaries function
 
 
 	### Function to create vnames assocaited with panel data fed to studentGrowthPercentiles and studentGrowthProjections functions
