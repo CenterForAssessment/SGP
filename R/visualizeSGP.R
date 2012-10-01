3995d2e6df50adab97c6b0e9cb662cb80552ff95
Damian Betebenner <dbetebenner@nciea.org>
2012-09-30 22:16:04 -0400

    Bug fix of state name calculation

--- R/visualizeSGP.R
+++ R/visualizeSGP.R
@@ -62,8 +62,8 @@ function(sgp_object,
 
 	if (missing(state)) {
 		tmp.name <- toupper(gsub("_", " ", deparse(substitute(sgp_object))))
-		if (any(sapply(c(state.name, "Demonstration", "AOB"), function(x) regexpr(toupper(x), tmp.name))!=1)) {
-			state <- c(state.abb, "DEMO", "AOB")[which(sapply(c(state.name, "Demonstration", "AOB"), function(x) regexpr(toupper(x), tmp.name))!=1)[1]]
+		if (any(sapply(c(state.name, "Demonstration", "AOB"), function(x) regexpr(toupper(x), tmp.name))!=-1)) {
+			state <- c(state.abb, "DEMO", "AOB")[which(sapply(c(state.name, "Demonstration", "AOB"), function(x) regexpr(toupper(x), tmp.name))!=-1)[1]]
 		}
 	}
 
