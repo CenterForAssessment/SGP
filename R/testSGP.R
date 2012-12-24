e86e45e015b208edf0c4700e8b8cda54a717ab94
Damian Betebenner <dbetebenner@nciea.org>
2012-12-24 09:50:29 -0500

    Bug fixes and refinements in testSGP

--- R/testSGP.R
+++ R/testSGP.R
@@ -22,7 +22,7 @@ function(
 
 	options(error=recover)
 	require(parallel)
-	Demonstration_SGP <- NULL
+	Demonstration_SGP <- tmp.messages <- NULL
 	number.cores <- detectCores()-1
 
 	expression.to.evaluate <- 
@@ -45,53 +45,56 @@ function(
 
 	### TEST of SGP variable
 
+	tmp.messages <- ("##### Results of testSGP test number 1 #####")
+
 	if (identical(sum(Demonstration_SGP@Data$SGP, na.rm=TRUE), 8565260L)) {
-		cat("Test of variable SGP: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP: OK\n")
 	} else {
-		cat("Test of variable SGP: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP: FAIL\n")
 	}
 
 	### TEST of SGP_BASELINE variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_BASELINE, na.rm=TRUE), 8573825L)) {
-		cat("Test of variable SGP_BASELINE: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_BASELINE: OK\n")
 	} else {
-		cat("Test of variable SGP_BASELINE: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_BASELINE: FAIL\n")
 	}
 
 	### TEST of SGP_TARGET variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_TARGET_3_YEAR, na.rm=TRUE), 7796624L)) {
-		cat("Test of variable SGP_TARGET_3_YEAR: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_3_YEAR: OK\n")
 	} else {
-		cat("Test of variable SGP_TARGET_3_YEAR: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_3_YEAR: FAIL\n")
 	}
 
 	### TEST of SGP_TARGET_MOVE_UP_STAY_UP variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_TARGET_MOVE_UP_STAY_UP, na.rm=TRUE), 9201802L)) {
-		cat("Test of variable SGP_TARGET_MOVE_UP_STAY_UP: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_MOVE_UP_STAY_UP: OK\n")
 	} else {
-		cat("Test of variable SGP_TARGET_MOVE_UP_STAY_UP: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_MOVE_UP_STAY_UP: FAIL\n")
 	}
 
 	### TEST of CATCH_UP_KEEP_UP_STATUS variable
 
 	if (identical(as.numeric(table(Demonstration_SGP@Data$CATCH_UP_KEEP_UP_STATUS)), c(41099, 10837, 35560, 84390))) {
-		cat("Test of variable CATCH_UP_KEEP_UP_STATUS: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable CATCH_UP_KEEP_UP_STATUS: OK\n")
 	} else {
-		cat("Test of variable CATCH_UP_KEEP_UP_STATUS: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable CATCH_UP_KEEP_UP_STATUS: FAIL\n")
 	}
 
 	### TEST of MOVE_UP_STAY_UP_STATUS variable
 
 	if (identical(as.numeric(table(Demonstration_SGP@Data$MOVE_UP_STAY_UP_STATUS)), c(72953, 15043, 18336, 13618))) {
-		cat("Test of variable MOVE_UP_STAY_UP_STATUS: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable MOVE_UP_STAY_UP_STATUS: OK\n")
 	} else {
-		cat("Test of variable MOVE_UP_STAY_UP_STATUS: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable MOVE_UP_STAY_UP_STATUS: FAIL\n")
 	}
 
-	cat("##### End testSGP test number 1 #####\n", fill=TRUE)
+	tmp.messages <- c(tmp.messages, "\n##### End testSGP test number 1 #####\n")
+	cat(tmp.messages, fill=TRUE)
 
 	} ### End TEST_NUMBER 1
 
@@ -106,12 +109,14 @@ function(
 
 	options(error=recover)
 	require(parallel)
-	Demonstration_SGP <- NULL
+	Demonstration_SGP <- tmp.messages <- NULL
 	Demonstration_Data_LONG <- subset(sgpData_LONG, YEAR %in% c("2007_2008", "2008_2009", "2009_2010", "2010_2011"))
 	Demonstration_Data_LONG_2011_2012 <- subset(sgpData_LONG, YEAR %in% c("2011_2012"))
 	number.cores <- detectCores()-1
 
-	cat("##### Beginning testSGP test number 2: Part 1 #####\n", fill=TRUE)
+	### Part 1
+
+	tmp.messages <- "\n##### Beginning testSGP test number 2: Part 1 #####\n"
 
 	expression.to.evaluate <- 
 		paste("Demonstration_SGP <- abcSGP(\n\tsgp_object=Demonstration_Data_LONG,\n\tsgPlot.demo.report=TRUE,\n\tparallel.config=list(BACKEND='PARALLEL', WORKERS=list(PERCENTILES=", number.cores, ", BASELINE_PERCENTILES=", number.cores, ", PROJECTIONS=", number.cores, ", LAGGED_PROJECTIONS=", number.cores, ", SUMMARY=", number.cores, ", GA_PLOTS=", number.cores, ", SG_PLOTS=1))\n)\n", sep="")
@@ -127,55 +132,59 @@ function(
 	### TEST of SGP variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP, na.rm=TRUE), 5668654L)) {
-		cat("Test of variable SGP, part 1: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP, part 1: OK\n")
 	} else {
-		cat("Test of variable SGP, part 1: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP, part 1: FAIL\n")
 	}
 
 	### TEST of SGP_BASELINE variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_BASELINE, na.rm=TRUE), 5667488L)) {
-		cat("Test of variable SGP_BASELINE, part 1: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_BASELINE, part 1: OK\n")
 	} else {
-		cat("Test of variable SGP_BASELINE, part 1: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_BASELINE, part 1: FAIL\n")
 	}
 
 	### TEST of SGP_TARGET_3_YEAR variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_TARGET_3_YEAR, na.rm=TRUE), 5245437L)) {
-		cat("Test of variable SGP_TARGET_3_YEAR, part 1: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_3_YEAR, part 1: OK\n")
 	} else {
-		cat("Test of variable SGP_TARGET_3_YEAR, part 1: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_3_YEAR, part 1: FAIL\n")
 	}
 
 	### TEST of SGP_TARGET_MOVE_UP_STAY_UP_3_YEAR variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_TARGET_MOVE_UP_STAY_UP_3_YEAR, na.rm=TRUE), 6088129L)) {
-		cat("Test of variable SGP_TARGET_MOVE_UP_STAY_UP_3_YEAR, part 1: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_MOVE_UP_STAY_UP_3_YEAR, part 1: OK\n")
 	} else {
-		cat("Test of variable SGP_TARGET_MOVE_UP_STAY_UP_3_YEAR, part 1: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_MOVE_UP_STAY_UP_3_YEAR, part 1: FAIL\n")
 	}
 
 	### TEST of CATCH_UP_KEEP_UP_STATUS variable
 
 	if (identical(as.numeric(table(Demonstration_SGP@Data$CATCH_UP_KEEP_UP_STATUS)), c(27122, 6990, 24358, 55283))) {
-		cat("Test of variable CATCH_UP_KEEP_UP_STATUS, part 1: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable CATCH_UP_KEEP_UP_STATUS, part 1: OK\n")
 	} else {
-		cat("Test of variable CATCH_UP_KEEP_UP_STATUS, part 1: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable CATCH_UP_KEEP_UP_STATUS, part 1: FAIL\n")
 	}
 
 	### TEST of MOVE_UP_STAY_UP_STATUS variable
 
-	if (identical(as.numeric(table(Demonstration_SGP@Data$MOVE_UP_STAY_UP_STATUS)), c(48152, 10393, 12150, 8943))) {
-		cat("Test of variable MOVE_UP_STAY_UP_STATUS, part 1: OK", fill=TRUE)
+	if (identical(as.numeric(table(Demonstration_SGP@Data$MOVE_UP_STAY_UP_STATUS)), c(48152, 10396, 12150, 8943))) {
+		tmp.messages <- c(tmp.messages, "\tTest of variable MOVE_UP_STAY_UP_STATUS, part 1: OK\n")
 	} else {
-		cat("Test of variable MOVE_UP_STAY_UP_STATUS, part 1: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable MOVE_UP_STAY_UP_STATUS, part 1: FAIL\n")
 	}
 
-	cat("##### Beginning testSGP test number 2: Part 2 #####\n", fill=TRUE)
+	tmp.messages <- c(tmp.messages, "\n##### End testSGP test number 1 #####\n")
+
+	### Part 2
+
+	tmp.messages <- c(tmp.messages, "\n##### Beginning testSGP test number 2: Part 2 #####\n")
 
 	expression.to.evaluate <- 
-		paste("Demonstration_SGP <- updateSGP(\n\twhat_sgp_object=Demonstration_SGP,\n\twith_sgp_data_LONG=Demonstration_Data_LONG_2011_2012\n\tsgPlot.demo.report=TRUE,\n\tparallel.config=list(BACKEND='PARALLEL', WORKERS=list(PERCENTILES=", number.cores, ", BASELINE_PERCENTILES=", number.cores, ", PROJECTIONS=", number.cores, ", LAGGED_PROJECTIONS=", number.cores, ", SUMMARY=", number.cores, ", GA_PLOTS=", number.cores, ", SG_PLOTS=1))\n)\n", sep="")
+		paste("Demonstration_SGP <- updateSGP(\n\twhat_sgp_object=Demonstration_SGP,\n\twith_sgp_data_LONG=Demonstration_Data_LONG_2011_2012,\n\tsgPlot.demo.report=TRUE,\n\tparallel.config=list(BACKEND='PARALLEL', WORKERS=list(PERCENTILES=", number.cores, ", BASELINE_PERCENTILES=", number.cores, ", PROJECTIONS=", number.cores, ", LAGGED_PROJECTIONS=", number.cores, ", SUMMARY=", number.cores, ", GA_PLOTS=", number.cores, ", SG_PLOTS=1))\n)\n", sep="")
 
 	cat(paste("EVALUATING:\n", expression.to.evaluate, sep=""), fill=TRUE)
 
@@ -188,50 +197,53 @@ function(
 	### TEST of SGP variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP, na.rm=TRUE), 8565260L)) {
-		cat("Test of variable SGP, part 2: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP, part 2: OK\n")
 	} else {
-		cat("Test of variable SGP, part 2: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP, part 2: FAIL\n")
 	}
 
 	### TEST of SGP_BASELINE variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_BASELINE, na.rm=TRUE), 8573825L)) {
-		cat("Test of variable SGP_BASELINE, part 2: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_BASELINE, part 2: OK\n")
 	} else {
-		cat("Test of variable SGP_BASELINE, part 2: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_BASELINE, part 2: FAIL\n")
 	}
 
 	### TEST of SGP_TARGET variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_TARGET_3_YEAR, na.rm=TRUE), 7796624L)) {
-		cat("Test of variable SGP_TARGET_3_YEAR, part 2: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_3_YEAR, part 2: OK\n")
 	} else {
-		cat("Test of variable SGP_TARGET_3_YEAR, part 2: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_3_YEAR, part 2: FAIL\n")
 	}
 
 	### TEST of SGP_TARGET_MOVE_UP_STAY_UP variable
 
 	if (identical(sum(Demonstration_SGP@Data$SGP_TARGET_MOVE_UP_STAY_UP, na.rm=TRUE), 9201802L)) {
-		cat("Test of variable SGP_TARGET_MOVE_UP_STAY_UP, part 2: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable SGP_TARGET_MOVE_UP_STAY_UP, part 2: OK\n")
 	} else {
-		cat("Test of variable SGP_TARGET_MOVE_UP_STAY_UP, part 2: FAIL", fill=TRUE)
+		tmp.messges <- c(tmp.messages, "\tTest of variable SGP_TARGET_MOVE_UP_STAY_UP, part 2: FAIL\n")
 	}
 
 	### TEST of CATCH_UP_KEEP_UP_STATUS variable
 
 	if (identical(as.numeric(table(Demonstration_SGP@Data$CATCH_UP_KEEP_UP_STATUS)), c(41099, 10837, 35560, 84390))) {
-		cat("Test of variable CATCH_UP_KEEP_UP_STATUS, part 2: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable CATCH_UP_KEEP_UP_STATUS, part 2: OK\n")
 	} else {
-		cat("Test of variable CATCH_UP_KEEP_UP_STATUS, part 2: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable CATCH_UP_KEEP_UP_STATUS, part 2: FAIL\n")
 	}
 
 	### TEST of MOVE_UP_STAY_UP_STATUS variable
 
 	if (identical(as.numeric(table(Demonstration_SGP@Data$MOVE_UP_STAY_UP_STATUS)), c(72953, 15043, 18336, 13618))) {
-		cat("Test of variable MOVE_UP_STAY_UP_STATUS, part 2: OK", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable MOVE_UP_STAY_UP_STATUS, part 2: OK\n")
 	} else {
-		cat("Test of variable MOVE_UP_STAY_UP_STATUS, part 2: FAIL", fill=TRUE)
+		tmp.messages <- c(tmp.messages, "\tTest of variable MOVE_UP_STAY_UP_STATUS, part 2: FAIL\n")
 	}
+
+	tmp.messages <- c(tmp.messages, "\n##### End testSGP test number 1 #####\n")
+	cat(tmp.messages, fill=TRUE)
 
 	} ### End TEST_NUMBER 2
 
