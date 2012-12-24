a22b9aab357980ec86b51851c335d0c047475f9e
Damian Betebenner <dbetebenner@nciea.org>
2012-12-24 07:26:17 -0500

    Update of updateSGP!

--- R/updateSGP.R
+++ R/updateSGP.R
@@ -6,43 +6,33 @@ function(what_sgp_object,
         started.at <- proc.time()
 	message(paste("\nStarted updateSGP", date()), "\n")
 
+
 	### Create state (if NULL) from sgp_object (if possible)
 
         if (is.null(state)) {
-		tmp.name <- toupper(gsub("_", " ", deparse(substitute(sgp_object))))
+		tmp.name <- toupper(gsub("_", " ", deparse(substitute(what_sgp_object))))
 		state <- getStateAbbreviation(tmp.name, "updateSGP")
         }
 
-	if (is.null(state)) {
-		tmp.name <- toupper(gsub("_", " ", deparse(substitute(what_sgp_object))))
-                tmp.name.position <- sapply(c(state.name, "AOB", "DEMONSTRATION"), function(x) regexpr(toupper(x), tmp.name))
-                if (any(tmp.name.position!=-1)) {
-                        state <- c(state.abb, "AOB", rep("DEMO", 2))[which(names(sort(tmp.name.position[tmp.name.position!=-1])[1])==c(state.name, "AOB", rep("DEMONSTRATION",2)))]
-		} else {
-			message("\tNOTE: Use of the higher level 'updateSGP' function requires extensive metadata embedded in the 'SGPstateData' list object. Please add your state's data to 'SGPstateData' by examining a state that is currently embedded. For example, SGPstateData[['DEMO']]. Please contact the package administrator with further questions.")
-		}
+
+	### Use prepareSGP on supplied 'with_sgp_data_LONG'
+
+	tmp_sgp_object <- prepareSGP(with_sgp_data_LONG, state=state, create.additional.variables=FALSE)
+	what_sgp_object@Data <- as.data.table(rbind.fill(what_sgp_object@Data, tmp_sgp_object@Data))
+	if ("HIGH_NEED_STATUS" %in% names(what_sgp_object@Data)) {
+		what_sgp_object@Data[['HIGH_NEED_STATUS']] <- NULL
+		what_sgp_object <- suppressMessages(prepareSGP(what_sgp_object, state=state))
 	}
 
 
-	### Use prepareSGP on supplied 'with_sgp_data_LONG' ###
+	### abcSGP
 
-		tmp_sgp_object <- prepareSGP(with_sgp_data_LONG, state=state, create.additional.variables=FALSE)
-		what_sgp_object@Data <- rbind.fill(what_sgp_object@Data, tmp_sgp_object@Data)
-		if ("HIGH_NEED_STATUS" %in% names(what_sgp_object@Data)) {
-			what_sgp_object@Data[['HIGH_NEED_STATUS']] <- NULL
-			what_sgp_object <- prepareSGP(what_sgp_object, state=state)
-		}
-
-
-	### abcSGP ###
-
-		new.year <- unique(tmp_sgp_object@Data$YEAR)
-		what_sgp_object <- abcSGP(what_sgp_object, years=new.year, ...)
+	new.year <- sort(unique(with_sgp_data_LONG$YEAR))
+	what_sgp_object <- abcSGP(what_sgp_object, years=new.year, ...)
 
 
 	### Print finish and return SGP object
 
         message(paste("Finished updateSGP", date(), "in", timetaken(started.at), "\n"))
 	return(what_sgp_object)
-
 } ## END updateSGP Function
