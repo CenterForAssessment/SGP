a08da0fa564312eb11a004c4872532503b9ead36
Damian Betebenner <dbetebenner@nciea.org>
2013-01-02 23:51:29 -0500

    Integration of coefficient matrix check into studentGrowthPercentiles

---
+++ R/unique.splineMatrix.R
@@ -0,0 +1,12 @@
+`unique.splineMatrix` <- 
+function(list.of.splineMatrices) {
+	tmp.list.1 <- lapply(list.of.splineMatrices, 
+		function(x) list(Content_Areas=x@Content_Areas, Grade_Progression=x@Grade_Progression, Time=x@Time, Time_Lags=x@Time_Lags))
+	tmp.list.2 <- lapply(list.of.splineMatrices, 
+		function(x) list(Content_Areas=x@Content_Areas, Grade_Progression=x@Grade_Progression, Time=x@Time, Time_Lags=x@Time_Lags, Version=x@Version))
+	if (any(duplicated(tmp.list.1))) {
+		list.of.splineMatrices[!duplicated(tmp.list.1[order(as.character(unlist(sapply(tmp.list.2, function(x) x$Version[1]))), decreasing=TRUE)])]
+	} else {
+		list.of.splineMatrices
+	}
+} ### END unique.splineMatrix
