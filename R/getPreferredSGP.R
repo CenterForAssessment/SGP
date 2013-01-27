4921b6c4b5164c7202540ed9c54d4b8277d9c576
dbetebenner <dbetebenner@gmail.com>
2013-01-27 06:38:28 -0500

    Refinement of message in getPreferredSGP

--- R/getPreferredSGP.R
+++ R/getPreferredSGP.R
@@ -1,6 +1,6 @@
 `getPreferredSGP` <- function(tmp.data, state) {
 	if (!is.null(SGPstateData[[state]][['SGP_Norm_Group_Preference']])) {
-		message("\tNOTE: Multiple SGPs exist for individual students. Unique SGPs will be created using SGP Norm Group Preference Table.")
+		message(paste("\tNOTE: Multiple SGPs exist for individual students. Unique SGPs will be created using SGP Norm Group Preference Table for ", state, ".", sep=""))
 	} else {
 		stop("\tNOTE: Multiple SGPs exist for individual students. Please examine results in @SGP[['SGPercentiles']].")
 	}
