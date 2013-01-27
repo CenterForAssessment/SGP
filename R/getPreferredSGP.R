4e54951cc4067776a8217e9df88bfbc273e36d65
dbetebenner <dbetebenner@gmail.com>
2013-01-27 05:18:50 -0500

    Bug fix to getPreferredSGP

--- R/getPreferredSGP.R
+++ R/getPreferredSGP.R
@@ -1,5 +1,5 @@
 `getPreferredSGP` <- function(tmp.data, state) {
-	if (!is.null(SGPstateData[[state]][['SGP_Norm_Group_Preference']]])) {
+	if (!is.null(SGPstateData[[state]][['SGP_Norm_Group_Preference']])) {
 		message("\tNOTE: Multiple SGPs exist for individual students. Unique SGPs will be created using SGP Norm Group Preference Table.")
 	} else {
 		stop("\tNOTE: Multiple SGPs exist for individual students. Please examine results in @SGP[['SGPercentiles']].")
