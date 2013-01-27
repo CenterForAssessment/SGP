05ee49946c0d53cb19a241506cfa137c897ab68c
dbetebenner <dbetebenner@gmail.com>
2013-01-27 13:12:21 -0500

    Bug fix to getPreferredSGP found in testing

--- R/getPreferredSGP.R
+++ R/getPreferredSGP.R
@@ -5,7 +5,7 @@
 		stop("\tNOTE: Multiple SGPs exist for individual students. Please examine results in @SGP[['SGPercentiles']].")
 	}
 	setkey(tmp.data, YEAR, SGP_NORM_GROUP)
-	tmp.data <- data.table(tmp.data[SGPstateData[[state]][['SGP_Norm_Group_Preference']]], key=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "PREFERENCE"))
+	tmp.data <- data.table(SGPstateData[[state]][['SGP_Norm_Group_Preference']][tmp.data], key=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "PREFERENCE"))
 	setkey(tmp.data, VALID_CASE, CONTENT_AREA, YEAR, ID)
-	tmp.data[!duplicated(tmp.data), -dim(tmp.data)[2], with=FALSE]
+	tmp.data[!duplicated(tmp.data)][,PREFERENCE:=NULL]
 } ### END getPreferredSGP
