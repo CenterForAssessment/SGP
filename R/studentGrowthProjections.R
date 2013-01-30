b3d4bbdb32e14113d0cc5bc8f6991bc82afe3d2e
dbetebenner <dbetebenner@gmail.com>
2013-01-30 07:51:02 -0500

    Added student group size to studentGrowthProjections

--- R/studentGrowthProjections.R
+++ R/studentGrowthProjections.R
@@ -597,7 +597,7 @@ function(panel.data,	## REQUIRED
 
 	if (print.time.taken) {
 	        message(paste("\tStarted studentGrowthProjections:", started.date))
-		message(paste("\tContent Area: ", sgp.labels$my.subject, ", Year: ", sgp.labels$my.year, ", Grade Progression: ", paste(grade.progression, collapse=", "), " ", sgp.labels$my.extra.label, sep="")) 
+		message(paste("\tContent Area: ", sgp.labels$my.subject, ", Year: ", sgp.labels$my.year, ", Grade Progression: ", paste(grade.progression, collapse=", "), " ", sgp.labels$my.extra.label, " (N=", dim(trajectories.and.cuts)[1], ")", sep="")) 
 		message(c(tmp.messages, "\tFinished studentGrowthProjections: ", date(), " in ", timetaken(started.at), "\n"))
 	} 
 
