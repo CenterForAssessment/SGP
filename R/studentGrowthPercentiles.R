8364f1a06981794a689a99e0e49ca90fdceebf0d
shangyi <shangyi@gmail.com>
2013-02-04 09:32:56 -0500

    simex update:set tmp.quantiles.simex as a list

--- R/studentGrowthPercentiles.R
+++ R/studentGrowthPercentiles.R
@@ -284,7 +284,7 @@ function(panel.data,         ## REQUIRED
 		year <- sgp.labels$my.year
 		my.path.knots.boundaries <- get.my.knots.boundaries.path(sgp.labels$my.subject, as.character(sgp.labels$my.year))
     
-		fitted <- extrap <- tmp.quantiles <- list()
+		fitted <- extrap <- tmp.quantiles.simex <- list()
 		loss.hoss <- matrix(nrow=2,ncol=length(tmp.gp)-1)
 		for (g in 1:ncol(loss.hoss)) {
 			loss.hoss[,g] <- SGPstateData[[state]][["Achievement"]][["Knots_Boundaries"]][[content_area]][[paste("loss.hoss_", rev(tmp.gp)[-1][g], sep="")]]
