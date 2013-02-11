1064fa40f56aac1c367e3e0fdd36359a83550aee
dbetebenner <dbetebenner@gmail.com>
2013-02-11 11:16:31 -0500

    Bug fix in checksplineMatrix

--- R/checksplineMatrix.R
+++ R/checksplineMatrix.R
@@ -4,7 +4,6 @@ function(list.of.splineMatrix, sgp_object=NULL) {
 		list.of.splineMatrix <- list(list.of.splineMatrix)
 	}
 	for (i in names(list.of.splineMatrix)) {
-		list.of.splineMatrix[[i]] <- unique.splineMatrix(list.of.splineMatrix[[i]])
 		splineMatrix.tf <- sapply(list.of.splineMatrix[[i]], validObject, test=TRUE)==TRUE
 		if (!all(splineMatrix.tf)) {
 			for (j in names(list.of.splineMatrix[[i]])[!splineMatrix.tf]) {
@@ -12,6 +11,7 @@ function(list.of.splineMatrix, sgp_object=NULL) {
 				list.of.splineMatrix[[i]][[j]] <- as.splineMatrix(matrix_argument=list.of.splineMatrix[[i]][[j]], matrix_argument_name=j, sgp_object=sgp_object)
 			}
 		}
+		list.of.splineMatrix[[i]] <- unique.splineMatrix(list.of.splineMatrix[[i]])
 	}
 	return(list.of.splineMatrix)
 } ### END checksplineMatrix
