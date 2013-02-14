4aa5c2f9850b0a929dee851f4dcd28160f1e5aaa
dbetebenner <dbetebenner@gmail.com>
2013-02-14 18:13:44 -0500

    Bug fix in getPreferredSGP

--- R/getPreferredSGP.R
+++ R/getPreferredSGP.R
@@ -21,7 +21,7 @@ function(tmp.data,
 	tmp.data <- data.table(SGPstateData[[state]][['SGP_Norm_Group_Preference']][,tmp.sgp.norm.group.variables,with=FALSE][tmp.data], 
 		key=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "PREFERENCE"))
 	setkey(tmp.data, VALID_CASE, CONTENT_AREA, YEAR, ID)
-	tmp.data[!duplicated(tmp.data)][,PREFERENCE:=NULL]
+	tmp.data <- tmp.data[!duplicated(tmp.data)][,PREFERENCE:=NULL]
 	setkeyv(tmp.data, getKey(tmp.data))
 	return(tmp.data)
 } ### END getPreferredSGP
