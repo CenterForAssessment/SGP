a6788b537a58966bb6a62e2982def8e9f19dc904
dbetebenner <dbetebenner@gmail.com>
2013-03-25 18:34:02 -0400

    First push of separate csemScoreSimulator

--- R/studentGrowthPercentiles.R
+++ R/studentGrowthPercentiles.R
@@ -241,36 +241,6 @@ function(panel.data,         ## REQUIRED
 		return(tmp)
 	} 
 
-	.csem.score.simulator <- function(scale_scores, grade, content_area, year, state, variable, distribution, round) {
-		GRADE <- CONTENT_AREA <- YEAR <- NULL
-		if (is.null(round)) round <- 1
-		if (is.null(distribution)) distribution <- "Normal"
-		if (!is.null(state)) min.max <- SGPstateData[[state]][["Achievement"]][["Knots_Boundaries"]][[content_area]][[paste("loss.hoss_", grade, sep="")]]
-		if (!is.null(variable)) min.max <- range(scale_scores, na.rm=TRUE)
-		if (!is.null(state)) {
-			if ("YEAR" %in% names(SGPstateData[[state]][["Assessment_Program_Information"]][["CSEM"]])) {
-				CSEM_Data <- subset(SGPstateData[[state]][["Assessment_Program_Information"]][["CSEM"]], GRADE==grade & CONTENT_AREA==content_area & YEAR==year)
-			} else {
-				CSEM_Data <- subset(SGPstateData[[state]][["Assessment_Program_Information"]][["CSEM"]], GRADE==grade & CONTENT_AREA==content_area)
-			}
-			CSEM_Function <- splinefun(CSEM_Data[["SCALE_SCORE"]], CSEM_Data[["SCALE_SCORE_CSEM"]], method="natural")
-			tmp.scale <- sqrt(CSEM_Function(scale_scores))
-		} 
-		if (!is.null(variable)) {
-			tmp.scale <- variable
-		}
-			if(distribution=="Skew-Normal") {
-				tmp.shape <- tan((pi/2)*((min.max[1]+min.max[2]) - 2*scale_scores)/(min.max[2]-min.max[1]))
-				tmp.score <- round_any(as.numeric(rsn(length(scale_scores), location=scale_scores, scale=tmp.scale, shape=tmp.shape)), round)
-			}
-			if(distribution=="Normal") {
-				tmp.score <- round_any(as.numeric(rnorm(length(scale_scores), mean=scale_scores, sd=tmp.scale)), round)
-			}
-			tmp.score[tmp.score < min.max[1]] <- min.max[1]
-			tmp.score[tmp.score > min.max[2]] <- min.max[2]
-			return(tmp.score)
-	}
-  
 	.simex.sgp <- function(state, variable, lambda, B, extrapolation) {
 		GRADE <- CONTENT_AREA <- YEAR <- V1 <- Lambda <- tau <- b <- PREDICTED_VALUES <- NULL ## To avoid R CMD check warnings
 		content_area <- sgp.labels$my.subject
@@ -972,7 +942,7 @@ function(panel.data,         ## REQUIRED
 						set.seed(k)
 						tmp.csem.quantiles[[j]][,TEMP_SGP_SIM:=.get.quantiles(
 								tmp.predictions, 
-								.csem.score.simulator(
+								csemScoreSimulator(
 									scale_scores=tmp.data[[dim(tmp.data)[2]]],
 									grade=tmp.last,
 									content_area=sgp.labels$my.subject,
