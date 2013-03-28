3b110441fd4f94d86e53ee70d2e360a8de13e9c0
dbetebenner <dbetebenner@gmail.com>
2013-03-28 09:50:58 -0400

    Update of getPreferredSGP to fix YEAR_WITHIN

--- R/getPreferredSGP.R
+++ R/getPreferredSGP.R
@@ -21,8 +21,8 @@ function(tmp.data,
 		stop("\tNOTE: Multiple SGPs exist for individual students. Please examine results in @SGP[['SGPercentiles']].")
 	}
 	tmp.data <- data.table(SGPstateData[[state]][['SGP_Norm_Group_Preference']][,tmp.sgp.norm.group.variables,with=FALSE][tmp.data], 
-		key=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "PREFERENCE"))
-	setkey(tmp.data, VALID_CASE, CONTENT_AREA, YEAR, ID)
+		key=c(getKey(tmp.data), "PREFERENCE"))
+	setkey(tmp.data, getKey(tmp.data))
 	tmp.data <- tmp.data[!duplicated(tmp.data)][,PREFERENCE:=NULL]
 	setkeyv(tmp.data, getKey(tmp.data))
 	return(tmp.data)
