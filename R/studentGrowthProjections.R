ebda62d21f83c91cc95026b8043781d678995f22
dbetebenner <dbetebenner@gmail.com>
2013-04-10 15:07:20 -0400

    Update to 1.1-0.0

--- R/studentGrowthProjections.R
+++ R/studentGrowthProjections.R
@@ -438,7 +438,12 @@ function(panel.data,	## REQUIRED
 	}
 
 	if (is.null(percentile.trajectory.values) & !tf.cutscores) {
-		stop("Either percentile trajectories and/or performance level cutscores must be supplied for the analyses.")
+		stop("\tNOTE: Either percentile trajectories and/or performance level cutscores must be supplied for the analyses.")
+	}
+
+	if (!all(percentile.trajectory.values) %in% 1:100) {
+		message("\tNOTE: Supplied 'percentile.trajectory.values' must be integers between 1 and 100. Only supplied values in that range will be used.")
+		percentile.trajectory.values <- intersect(percentile.trajectory.values, 1:100)
 	}
 
 	if (!is.null(achievement.level.prior.vname)) {
