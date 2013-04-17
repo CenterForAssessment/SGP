d7e31dc6c5f2ee6eead1a72db259468d3a508a0b
dbetebenner <dbetebenner@gmail.com>
2013-04-17 07:17:20 -0400

    Update of SGPstateData and getCohortData functionality

--- R/getCohortDataInfo.R
+++ R/getCohortDataInfo.R
@@ -1,9 +1,9 @@
 `getCohortDataInfo` <- 
 function(sgp.data,
-	sgp.config) {
+	par.sgp.config) {
 
-
-	return("Under Construction")
-
-
+	for (sgp.iter in seq(length(par.sgp.config))) {
+		par.sgp.config[[sgp.iter]][['panel_data_dimension']] <- dim(getPanelData(tmp_sgp_data_for_analysis, "sgp.percentiles", par.sgp.config[sgp.iter]))
+	}
+	return(par.sgp.config)
 } ### END getCohortDataInfo
