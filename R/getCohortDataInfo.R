899baf572e329ee60f37f4120c348fd570c7c097
dbetebenner <dbetebenner@gmail.com>
2013-04-18 17:05:21 -0400

    Update of getCohortDataInfo

--- R/getCohortDataInfo.R
+++ R/getCohortDataInfo.R
@@ -3,6 +3,8 @@ function(sgp.data,
 	par.sgp.config) {
 
 	for (sgp.iter in seq(length(par.sgp.config))) {
+		tmp.grades <- paste("GRADE", tail(rev(par.sgp.config[[sgp.iter]][['sgp.panel.years']]), 2), tail(rev(par.sgp.config[[sgp.iter]][['sgp.content.areas']]), 2), sep=".")
+		tmp.scale.scores <- paste("GRADE", tail(rev(par.sgp.config[[sgp.iter]][['sgp.panel.years']]), 2), tail(rev(par.sgp.config[[sgp.iter]][['sgp.content.areas']]), 2), sep=".")
 		par.sgp.config[[sgp.iter]][['panel_data_dimension']] <- dim(getPanelData(sgp.data, "sgp.percentiles", par.sgp.config[[sgp.iter]]))
 	}
 	return(par.sgp.config)
