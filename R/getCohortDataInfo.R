5bd55befabeec7e6fd6a3f1e720dcfb2b88521a7
dbetebenner <dbetebenner@gmail.com>
2013-04-19 09:58:46 -0400

    Update of getCohortDataInfo

--- R/getCohortDataInfo.R
+++ R/getCohortDataInfo.R
@@ -3,9 +3,15 @@ function(sgp.data,
 	par.sgp.config) {
 
 	for (sgp.iter in seq(length(par.sgp.config))) {
-		tmp.grades <- paste("GRADE", tail(rev(par.sgp.config[[sgp.iter]][['sgp.panel.years']]), 2), tail(rev(par.sgp.config[[sgp.iter]][['sgp.content.areas']]), 2), sep=".")
-		tmp.scale.scores <- paste("GRADE", tail(rev(par.sgp.config[[sgp.iter]][['sgp.panel.years']]), 2), tail(rev(par.sgp.config[[sgp.iter]][['sgp.content.areas']]), 2), sep=".")
-		par.sgp.config[[sgp.iter]][['panel_data_dimension']] <- dim(getPanelData(sgp.data, "sgp.percentiles", par.sgp.config[[sgp.iter]]))
+		tmp.grades <- paste(
+				paste(
+					paste("GRADE", tail(par.sgp.config[[sgp.iter]][['sgp.panel.years']], 2), tail(par.sgp.config[[sgp.iter]][['sgp.content.areas']], 2), sep="."),
+					"=='", tail(par.sgp.config[[sgp.iter]][['sgp.grade.sequences']][[1]], 2), "'", sep=""), collapse=" & ")
+		tmp.scale.scores <- paste(
+					paste("!is.na(",
+						paste("SCALE_SCORE", tail(rev(par.sgp.config[[sgp.iter]][['sgp.panel.years']]), 2), tail(rev(par.sgp.config[[sgp.iter]][['sgp.content.areas']]), 2), sep="."), ")", sep=""), collapse=" & ")
+		par.sgp.config[[sgp.iter]][['panel_data_dimension']] <- 
+			dim(as.data.table(getPanelData(sgp.data, "sgp.percentiles", par.sgp.config[[sgp.iter]]))[eval(parse(text=paste(tmp.grades, tmp.scale.scores, sep=" & ")))])
 	}
 	return(par.sgp.config)
 } ### END getCohortDataInfo
