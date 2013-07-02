6d063d3f1f675fc2cc125d1060effa9425a4cdb7
Adam VanIwaarden <avaniwaarden@gmail.com>
2013-07-02 11:15:55 -0500

    SEGFAULT ISSUE:  Get rid of assign by reference and do a join/merge instead

--- R/combineSGP.R
+++ R/combineSGP.R
@@ -126,9 +126,7 @@ function(
 			slot.data <- tmp.data[slot.data]
 		} else {
 			variables.to.merge <- names(tmp.data) %w/o% key(slot.data)
-			for (tmp.merge.variable in variables.to.merge) {
-				slot.data[tmp.data[,key(slot.data), with=FALSE], tmp.merge.variable := tmp.data[,tmp.merge.variable, with=FALSE], with=FALSE, nomatch=0]
-			}
+			slot.data <- tmp.data[,c(key(slot.data), variables.to.merge), with=FALSE][slot.data]
 		}
 
 		setkeyv(slot.data, getKey(slot.data))
@@ -173,10 +171,8 @@ function(
 		if (length(intersect(names(slot.data), names(tmp.data)) %w/o% key(slot.data))==0) {
 			slot.data <- tmp.data[slot.data]
 		} else {
-			variables.to.merge <- names(tmp.data) %w/o% c(key(slot.data), "SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED")
-			for (tmp.merge.variable in variables.to.merge) {
-				slot.data[tmp.data[,key(slot.data), with=FALSE], tmp.merge.variable := tmp.data[, tmp.merge.variable, with=FALSE], with=FALSE, nomatch=0]
-			}
+			variables.to.merge <- names(tmp.data) %w/o% names(slot.data)
+			slot.data <- tmp.data[,c(key(slot.data), variables.to.merge), with=FALSE][slot.data]
 		}
 
 		setkeyv(slot.data, getKey(slot.data))
