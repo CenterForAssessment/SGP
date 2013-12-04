4645bf6fcb588e4e12080d223ff1273edefdf54f
Adam VanIwaarden <avaniwaarden@gmail.com>
2013-12-04 11:19:22 -0600

    Modify what col.index is looking for so that it gets the right column (was finding anything with GRADE in th name, including GRADE_9_LIT)

--- R/baselineSGP.R
+++ R/baselineSGP.R
@@ -89,7 +89,7 @@ function(sgp_object,
 			tmp.sgp.iter$sgp.panel.years <- tmp.year.sequence[[k]]
 			tmp.sgp.iter$sgp.grade.sequences <- tmp.sgp.iter$sgp.grade.sequences
 			tmp.list[[k]] <- getPanelData(tmp_sgp_data_for_analysis, "sgp.percentiles", sgp.iter = tmp.sgp.iter)
-			col.index <- c(1, grep("GRADE",names(tmp.list[[k]])), grep("SCALE_SCORE",names(tmp.list[[k]])))
+			col.index <- c(1, grep("GRADE.",names(tmp.list[[k]])), grep("SCALE_SCORE.",names(tmp.list[[k]])))
 			tmp.list[[k]] <- tmp.list[[k]][!apply(is.na(tmp.list[[k]]), 1, any), col.index]
 			names(tmp.list[[k]]) <- c("ID", paste("GRADE", rev(seq_along(tmp.year.sequence[[k]])), sep="_"), 
 				paste("SCALE_SCORE", rev(seq_along(tmp.year.sequence[[k]])), sep="_")) # Use rev() in case vars are added: 1= current, 2= first prior, etc.
