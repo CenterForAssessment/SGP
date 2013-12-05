46215d2f18f346ce47448173ab29ecd49d0cf9eb
Adam VanIwaarden <avaniwaarden@gmail.com>
2013-12-05 00:01:15 -0600

    Use getPanelDataVnames function to order tmp.list[[k]] data.frame correctly

--- R/baselineSGP.R
+++ R/baselineSGP.R
@@ -88,9 +88,8 @@ function(sgp_object,
 			names(tmp.sgp.iter) <- gsub('sgp.baseline.', 'sgp.', names(tmp.sgp.iter))
 			tmp.sgp.iter$sgp.panel.years <- tmp.year.sequence[[k]]
 			tmp.sgp.iter$sgp.grade.sequences <- tmp.sgp.iter$sgp.grade.sequences
-			tmp.list[[k]] <- getPanelData(tmp_sgp_data_for_analysis, "sgp.percentiles", sgp.iter = tmp.sgp.iter)
-			col.index <- c(1, grep("GRADE[.]",names(tmp.list[[k]])), grep("SCALE_SCORE[.]",names(tmp.list[[k]])))
-			tmp.list[[k]] <- tmp.list[[k]][!apply(is.na(tmp.list[[k]]), 1, any), col.index]
+			tmp.list[[k]] <- getPanelData(tmp_sgp_data_for_analysis, "sgp.percentiles", sgp.iter = tmp.sgp.iter)[,
+				getPanelDataVnames("sgp.percentiles", tmp.sgp.iter, names(tmp_sgp_data_for_analysis))]
 			names(tmp.list[[k]]) <- c("ID", paste("GRADE", rev(seq_along(tmp.year.sequence[[k]])), sep="_"), 
 				paste("SCALE_SCORE", rev(seq_along(tmp.year.sequence[[k]])), sep="_")) # Use rev() in case vars are added: 1= current, 2= first prior, etc.
 		}
