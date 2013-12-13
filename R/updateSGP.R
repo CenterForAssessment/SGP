fcef30e8e921b87aa5a078506c2ea1b50e19e330
Adam VanIwaarden <avaniwaarden@gmail.com>
2013-12-13 06:13:15 -0600

    Enforce that return.prior.scale.score.standardized is FALSE for additional case update

--- R/updateSGP.R
+++ R/updateSGP.R
@@ -180,6 +180,8 @@ function(what_sgp_object=NULL,
 				}
 				tmp.sgp_object.update <- prepareSGP(tmp.long.data, state=state, create.additional.variables=FALSE)
 				tmp.sgp_object.update@SGP$Coefficient_Matrices <- what_sgp_object@SGP$Coefficient_Matrices
+				
+				SGPstateData[[state]][["SGP_Configuration"]][["return.prior.scale.score.standardized"]] <- FALSE
 
 				tmp.sgp_object.update <- analyzeSGP(
 							tmp.sgp_object.update,
