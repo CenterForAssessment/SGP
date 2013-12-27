063e137e77ac695b84f5d59d8e0b7b03cffa99ad
dbetebenner <dbetebenner@gmail.com>
2013-12-27 05:51:38 -0500

    Update of combineSGP, simplifying joins that occur

--- R/combineSGP.R
+++ R/combineSGP.R
@@ -172,13 +172,13 @@ function(
 			tmp.data <- getPreferredSGP(tmp.data, state)
 		}
 
-		if (length(intersect(names(slot.data), names(tmp.data)) %w/o% key(slot.data))==0) {
-			slot.data <- tmp.data[slot.data]
-		} else {
+#		if (length(intersect(names(slot.data), names(tmp.data)) %w/o% key(slot.data))==0) {
+#			slot.data <- tmp.data[slot.data]
+#		} else {
 			variables.to.merge <- names(tmp.data) %w/o% key(slot.data)
 			tmp.index <- slot.data[tmp.data[,key(slot.data), with=FALSE], which=TRUE]
 			slot.data[tmp.index, variables.to.merge := tmp.data[, variables.to.merge, with=FALSE], with=FALSE, nomatch=0]
-		}
+#		}
 
 		setkeyv(slot.data, getKey(slot.data))
 		rm(tmp.list); suppressMessages(gc())
@@ -221,13 +221,13 @@ function(
 			tmp.data <- getPreferredSGP(tmp.data, state, type="BASELINE")
 		}
 
-		if (length(intersect(names(slot.data), names(tmp.data)) %w/o% c(key(slot.data), "SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED"))==0) {
-			slot.data <- tmp.data[,!c("SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED"), with=FALSE][slot.data]
-		} else {
+#		if (length(intersect(names(slot.data), names(tmp.data)) %w/o% c(key(slot.data), "SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED"))==0) {
+#			slot.data <- tmp.data[,!c("SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED"), with=FALSE][slot.data]
+#		} else {
 			variables.to.merge <- names(tmp.data) %w/o% c(key(slot.data), "SCALE_SCORE_PRIOR", "SCALE_SCORE_PRIOR_STANDARDIZED")
 			tmp.index <- slot.data[tmp.data[,key(slot.data), with=FALSE], which=TRUE]
 			slot.data[tmp.index, variables.to.merge := tmp.data[, variables.to.merge, with=FALSE], with=FALSE, nomatch=0]
-		}
+#		}
 
 		setkeyv(slot.data, getKey(slot.data))
 		rm(tmp.list); suppressMessages(gc())
