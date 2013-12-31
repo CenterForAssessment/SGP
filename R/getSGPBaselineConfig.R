0e9379689c18a719a588ea4108bbba3f6ce16289
Adam VanIwaarden <avaniwaarden@gmail.com>
2013-12-31 09:09:41 -0600

    Add sgp.percentiles.baseline.max.order argument to getSGPBaselineConfig and move .grades construction to be content area specific

--- R/getSGPBaselineConfig.R
+++ R/getSGPBaselineConfig.R
@@ -2,7 +2,8 @@
 function(sgp_object, 
 	content_areas,
 	grades, 
-	sgp.baseline.panel.years) {
+	sgp.baseline.panel.years,
+	sgp.percentiles.baseline.max.order) {
 
 	sgp.baseline.config <- tmp.sgp.baseline.config <- .content_areas <- .years <- .grades <- .sgp.grade.sequences <- list()
 
@@ -18,25 +19,25 @@ function(sgp_object,
 		.years <- sgp.baseline.panel.years
 	}
 
-	.baseline.max.order <- length(.years)-2
-
-	.grades <- sort(type.convert(unique(sgp_object@Data[SJ("VALID_CASE", .content_areas)][["GRADE"]])))
-	tmp.sgp.grade.sequences <- lapply(.grades[-1], function(x) tail(.grades[.grades <= x], (.baseline.max.order+1)))
-	tmp.sgp.baseline.grade.sequences <- sapply(tmp.sgp.grade.sequences, function(x) x[(tail(x,1)-x) <= .baseline.max.order])
-	if (!is.null(grades)) {
-		tmp.sgp.baseline.grade.sequences <- tmp.sgp.baseline.grade.sequences[sapply(tmp.sgp.baseline.grade.sequences, function(x) tail(x, 1) %in% grades)]
-	}
-
-	sgp.baseline.grade.sequences <- list()
-	for (a in seq_along(tmp.sgp.baseline.grade.sequences)) {
-		sgp.baseline.grade.sequences[[a]] <-
-			eval(parse(text=paste("list(", paste("tail(tmp.sgp.baseline.grade.sequences[[", a, "]],", length(tmp.sgp.baseline.grade.sequences[[a]]):2, ")", collapse=", "), ")")))
-	}
-
-	sgp.baseline.grade.sequences <- unlist(sgp.baseline.grade.sequences, recursive=FALSE)
-	sgp.baseline.grade.sequences.lags <- lapply(sgp.baseline.grade.sequences, diff)
+	.baseline.max.order <- min(sgp.percentiles.baseline.max.order, length(.years)-2)
 
 	for (i in .content_areas) {
+		.grades <- sort(type.convert(unique(sgp_object@Data[SJ("VALID_CASE", i)][["GRADE"]])))
+		tmp.sgp.grade.sequences <- lapply(.grades[-1], function(x) tail(.grades[.grades <= x], (.baseline.max.order+1)))
+		tmp.sgp.baseline.grade.sequences <- sapply(tmp.sgp.grade.sequences, function(x) x[(tail(x,1)-x) <= .baseline.max.order])
+		if (!is.null(grades)) {
+			tmp.sgp.baseline.grade.sequences <- tmp.sgp.baseline.grade.sequences[sapply(tmp.sgp.baseline.grade.sequences, function(x) tail(x, 1) %in% grades)]
+		}
+	
+		sgp.baseline.grade.sequences <- list()
+		for (a in seq_along(tmp.sgp.baseline.grade.sequences)) {
+			sgp.baseline.grade.sequences[[a]] <-
+				eval(parse(text=paste("list(", paste("tail(tmp.sgp.baseline.grade.sequences[[", a, "]],", length(tmp.sgp.baseline.grade.sequences[[a]]):2, ")", collapse=", "), ")")))
+		}
+	
+		sgp.baseline.grade.sequences <- unlist(sgp.baseline.grade.sequences, recursive=FALSE)
+		sgp.baseline.grade.sequences.lags <- lapply(sgp.baseline.grade.sequences, diff)
+
 		tmp.sgp.baseline.config[[as.character(i)]] <- 
 			list(
 				sgp.baseline.content.areas=i, 
