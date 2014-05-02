c3cb8f474bd1df01cae8deef0e1ebb6b03b06407
dbetebenner <dbetebenner@gmail.com>
2014-05-02 13:19:30 -0400

    Bug fix from latest getTargetSGPLevel commits

--- R/getTargetSGPLevel.R
+++ R/getTargetSGPLevel.R
@@ -5,23 +5,24 @@ function(state,
 		if (!is.null(SGPstateData[[state]][['Achievement']][['Cutscore_Information']])) {
 			tmp.state.level <- which(sapply(lapply(SGPstateData[["RLI"]][["Achievement"]][["Cutscore_Information"]][['State_Levels']], '[[', 1), function(x) state.iter %in% x[[1]]))
 			if (target.level=="CATCH_UP_KEEP_UP") {
-				level.to.get <- which.max(SGPstateData[[state.iter]][["Achievement"]][["Cutscore_Information"]][['State_Levels']][[tmp.state.level]][['Levels']]=="Proficient")-1
+				level.to.get <- which.max(SGPstateData[[state]][["Achievement"]][["Cutscore_Information"]][['State_Levels']][[tmp.state.level]][['Levels']]=="Proficient")-1
 			}
 			if (target.level=="MOVE_UP_STAY_UP") {
-				if (length(which(SGPstateData[[state.iter]][["Achievement"]][["Cutscore_Information"]][['State_Levels']][[tmp.state.level]][['Levels']]=="Proficient")) <= 1) {
-					stop(paste("\tNOTE: MOVE_UP_STAY_UP Targets cannot be calculated because no achievement levels above PROFICIENT exist in ", state.iter, ".", sep=""))  
+				if (length(which(SGPstateData[[state]][["Achievement"]][["Cutscore_Information"]][['State_Levels']][[tmp.state.level]][['Levels']]=="Proficient")) <= 1) {
+					stop(paste("\tNOTE: MOVE_UP_STAY_UP Targets cannot be calculated because no achievement levels above PROFICIENT exist in ", state, "/", state.iter, ".", sep=""))  
 				} else {
-					level.to.get <- which.max(SGPstateData[[state.iter]][["Achievement"]][["Cutscore_Information"]][['State_Levels']][[tmp.state.level]][['Levels']]=="Proficient")
+					level.to.get <- which.max(SGPstateData[[state]][["Achievement"]][["Cutscore_Information"]][['State_Levels']][[tmp.state.level]][['Levels']]=="Proficient")
 				}
 			}
 		} else {
-			if (target.level=="CATCH_UP_KEEP_UP") level.to.get <- which.max(SGPstateData[[state.iter]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient")-1
+			if (target.level=="CATCH_UP_KEEP_UP") level.to.get <- which.max(SGPstateData[[state]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient")-1
 			if (target.level=="MOVE_UP_STAY_UP") {
-				if (length(which(SGPstateData[[state.iter]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient")) <= 1) {
-					stop(paste("\tNOTE: MOVE_UP_STAY_UP Targets cannot be calculated because no achievement levels above PROFICIENT exist in ", state.iter, ".", sep=""))  
+				if (length(which(SGPstateData[[state]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient")) <= 1) {
+					stop(paste("\tNOTE: MOVE_UP_STAY_UP Targets cannot be calculated because no achievement levels above PROFICIENT exist in ", state, ".", sep=""))  
 				} else {
-					level.to.get <- which.max(SGPstateData[[state.iter]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient")
+					level.to.get <- which.max(SGPstateData[[state]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient")
 				}
 			}
-		} 
+		}
+		return(level.to.get) 
 } ### END getTargetSGPLevel
