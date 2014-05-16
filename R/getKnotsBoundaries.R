20d32d8f52a42ba5a908d0ae7ed8fa2cc490c170
dbetebenner <dbetebenner@gmail.com>
2014-05-16 08:12:51 -0400

    Update of getKnotsBoundaries

--- R/getKnotsBoundaries.R
+++ R/getKnotsBoundaries.R
@@ -10,11 +10,19 @@ function(sgp.iter,
 		}
 		kb <- list()
 		tmp.content_areas <- unique(sgp.iter[[my.content.areas]])
-		if (!is.null(SGPstateData[[state]][["SGP_Configuration"]][["content_area.projection.sequence"]]) && sgp.iter.type[2] %in% c("sgp.projections", "sgp.projections.lagged")) {
+		if (!is.null(SGPstateData[[state]][["SGP_Configuration"]][["content_area.projection.sequence"]]) & sgp.iter.type[2] %in% c("sgp.projections", "sgp.projections.lagged")) {
 			tmp.content_areas <- unique(unlist(sapply(tmp.content_areas, function(x) unique(SGPstateData[[state]][["SGP_Configuration"]][["content_area.projection.sequence"]][[x]]))))
 		}
-		content_area.label <- tail(sgp.iter[["sgp.projection.content.areas"]], 1)
-		tmp.year <- tail(sgp.iter[["sgp.panel.years"]], 1)
+		if (sgp.iter.type[2]=="sgp.projections") {
+			content_area.label <- tail(sgp.iter[["sgp.projection.content.areas"]], 1)
+		} else {
+			content_area.label <- tail(sgp.iter[["sgp.content.areas"]], 1)
+		}
+		if (sgp.iter.type[2]=="sgp.projections") {
+			tmp.year <- tail(sgp.iter[["sgp.projection.panel.years"]], 1)
+		} else {
+			tmp.year <- tail(sgp.iter[["sgp.panel.years"]], 1)
+		}
 
 		for (j in tmp.content_areas) {
 			for (i in grep(j, names(SGPstateData[[state]][["Achievement"]][["Knots_Boundaries"]]), value=TRUE)) {
@@ -33,7 +41,11 @@ function(sgp.iter,
 		if (!is.null(SGPstateData[[state]][["SGP_Configuration"]][["content_area.projection.sequence"]]) & sgp.iter.type[2] %in% c("sgp.projections", "sgp.projections.lagged")) {
 			tmp.content_areas <- unique(unlist(sapply(tmp.content_areas, function(x) unique(SGPstateData[[state]][["SGP_Configuration"]][["content_area.projection.sequence"]][[x]]))))
 		}
-		content_area.label <- tail(sgp.iter[["sgp.projection.content.areas"]], 1)
+		if (sgp.iter.type[2]=="sgp.projections") {
+			content_area.label <- tail(sgp.iter[["sgp.projection.content.areas"]], 1)
+		} else {
+			content_area.label <- tail(sgp.iter[["sgp.content.areas"]], 1)
+		}
 		tmp.year <- "BASELINE"
 
 		for (j in tmp.content_areas) {
