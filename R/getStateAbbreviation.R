c7bc85055c7499565ccc0197b131e86a44977006
Adam VanIwaarden <avaniwaarden@gmail.com>
2014-05-23 14:34:11 -0500

    Add Colorado WIDA to state abbreviation

--- R/getStateAbbreviation.R
+++ R/getStateAbbreviation.R
@@ -5,7 +5,7 @@ function(
 	type="Abbreviation") {
 
 	my.state.abbreviations <- c(state.abb, "ABQ", "AOB", rep("DEMO", 2), "GUA", "RLI", "WIDA") ### NOTE: Add abbreviations ALPHABETICALLY
-	my.state.names <- c(state.name, "ALBUQUERQUE", "AOB", "DEMONSTRATION", "SGPDATA LONG", "GUATEMALA", "RLI", "WIDA") ### NOTE: Add state names ALPHABETICAALY
+	my.state.names <- c(state.name, "ALBUQUERQUE", "AOB", "DEMONSTRATION", "SGPDATA LONG", "GUATEMALA", "RLI", "WIDA", "WIDA_CO", "WIDA_MA") ### NOTE: Add state names ALPHABETICAALY
 	if (type=="Abbreviation") {
 		tmp.name.position <- sapply(my.state.names, function(x) regexpr(toupper(x), supplied.name))
 	} else {
