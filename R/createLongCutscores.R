027575738e56956854e3eb739fdddc811f548635
dbetebenner <dbetebenner@gmail.com>
2014-11-24 06:15:21 -0500

    More updates

--- R/createLongCutscores.R
+++ R/createLongCutscores.R
@@ -139,7 +139,6 @@ function(state,
 		setkeyv(grades.content_areas.reported.in.state, c("GRADE", "CONTENT_AREA"))
 		tmp.long.cutscores <- grades.content_areas.reported.in.state[tmp.long.cutscores]
 		tmp.long.cutscores[,YEAR_LAG:=NULL]
-		setkeyv(tmp.long.cutscores, c("GRADE_NUMERIC", "CONTENT_AREA"))
 	}
 
 	return(tmp.long.cutscores)
