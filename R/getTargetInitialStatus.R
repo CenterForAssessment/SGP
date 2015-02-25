1c2e21f8f7d5271b25da0e759ebd7196a8b83944
dbetebenner <dbetebenner@gmail.com>
2015-02-25 13:32:05 -0500

    Adding MOVE_UP_STAY_UP analyses to assessment transition

--- R/getTargetInitialStatus.R
+++ R/getTargetInitialStatus.R
@@ -14,11 +14,21 @@ function(achievement_level,
 				which(SGPstateData[[state]][["Achievement"]][["Cutscore_Information"]][["State_Levels"]][[tmp.state.level]][['Levels']]=="Not Proficient")]
 		} else {
 			if (!is.null(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]])) {
+				levels.that.are.advanced <- levels.that.are.not.advanced <- list()
 				tmp.index <- grep("Achievement_Levels", names(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]]))
 				levels.that.are.proficient <- sort(as.vector(sapply(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][tmp.index], function(x) x[['Labels']]))[
 					as.vector(sapply(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][tmp.index], function(x) x[['Proficient']]))=="Proficient"])
 				levels.that.are.not.proficient <- sort(as.vector(sapply(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][tmp.index], function(x) x[['Labels']]))[
 					as.vector(sapply(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][tmp.index], function(x) x[['Proficient']]))=="Not Proficient"])
+				for (i in grep("Achievement_Levels", names(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]]), value=TRUE)) {
+					levels.that.are.advanced[[i]] <- SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][[i]][['Labels']][
+						tail(which(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][[i]][["Proficient"]]=="Proficient"), -1)]
+					levels.that.are.not.advanced[[i]] <- SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][[i]][['Labels']][
+						c(which(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][[i]][["Proficient"]]=="Not Proficient"),
+						head(which(SGPstateData[[state]][["Assessment_Program_Information"]][["Assessment_Transition"]][[i]][["Proficient"]]=="Proficient"), 1))]
+				}
+				levels.that.are.advanced <- unlist(levels.that.are.advanced)
+				levels.that.are.not.advanced <- unlist(levels.that.are.not.advanced)
 			} else {
 				levels.that.are.proficient <- SGPstateData[[state]][["Achievement"]][["Levels"]][["Labels"]][
 					which(SGPstateData[[state]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient")]
@@ -29,7 +39,6 @@ function(achievement_level,
 				levels.that.are.not.advanced <- SGPstateData[[state]][["Achievement"]][["Levels"]][["Labels"]][
 					c(which(SGPstateData[[state]][["Achievement"]][["Levels"]][["Proficient"]]=="Not Proficient"),
 					head(which(SGPstateData[[state]][["Achievement"]][["Levels"]][["Proficient"]]=="Proficient"), 1))]
-
 			}
 		}
 
