aa44b0f13fb20832a71c50fead5dd36498d8f708
dbetebenner <dbetebenner@gmail.com>
2015-08-28 08:55:01 -0400

    Bumping version # to 1.3-9.50

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <- 
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('SGP ',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (8-27-2015).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki')
+		packageStartupMessage('SGP ',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (8-28-2015).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki')
 	}
 }
