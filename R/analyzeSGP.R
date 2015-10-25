f12e403d8794d4dd1fee956cabfe7cafd3861823
dbetebenner <dbetebenner@gmail.com>
2015-10-25 16:58:39 -0400

    Adjusting path for output

--- R/analyzeSGP.R
+++ R/analyzeSGP.R
@@ -397,12 +397,12 @@ function(sgp_object,
                         !is.na(SCALE_SCORE), intersect(names(data.for.equate),
                             c("CONTENT_AREA", "YEAR", "GRADE", "SCALE_SCORE", "SCALE_SCORE_ACTUAL", paste("SCALE_SCORE)_EQUATED", sgp.percentiles.equating.method.iter, conversion.type.iter, sep="_"))), with=FALSE]
                     write.table(Scale_Score_Linkages[[conversion.type.iter]][[toupper(sgp.percentiles.equating.method.iter)]],
-                        file=paste(paste(gsub(" ", "_", getStateAbbreviation(state, type="LONG")), "Scale_Score_Linkages", toupper(sgp.percentiles.equating.method.iter), conversion.type.iter, sep="_"), "txt", sep="."), row.names=FALSE, na="", quote=FALSE, sep="|")
+                        file=paste("Data/Linkages/", paste(gsub(" ", "_", getStateAbbreviation(state, type="LONG")), "Scale_Score_Linkages", toupper(sgp.percentiles.equating.method.iter), conversion.type.iter, sep="_"), ".txt", sep=""), row.names=FALSE, na="", quote=FALSE, sep="|")
                 }
             }
             assign(paste(gsub(" ", "_", getStateAbbreviation(state, type="LONG")), "Scale_Score_Linkages", sep="_"), Scale_Score_Linkages)
             save(list=paste(gsub(" ", "_", getStateAbbreviation(state, type="LONG")), "Scale_Score_Linkages", sep="_"),
-                file=paste(paste(gsub(" ", "_", getStateAbbreviation(state, type="LONG")), "Scale_Score_Linkages", sep="_"), "Rdata", sep="."))
+                file=paste("Data/Linkages/", paste(gsub(" ", "_", getStateAbbreviation(state, type="LONG")), "Scale_Score_Linkages", sep="_"), ".Rdata", sep=""))
             setkey(data.for.equate, VALID_CASE, CONTENT_AREA, YEAR, ID)
             sgp_object@Data <- data.for.equate[,c(names(sgp_object@Data, 'SCALE_SCORE_EQUATED_EQUIPERCENTILE_OLD_TO_NEW'))][,SCALE_SCORE_EQUATED:=SCALE_SCORE_EQUATED_EQUIPERCENTILE_OLD_TO_NEW]
         }
