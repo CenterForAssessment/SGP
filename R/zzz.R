1d5032e588609f88fedf79e7b0034313f6b4167f
dbetebenner <dbetebenner@gmail.com>
2015-11-01 04:23:58 -0500

    Bumping date and version number

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <- 
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('SGP ',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (10-30-2015).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki')
+		packageStartupMessage('SGP ',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (11-1-2015).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki')
 	}
 }
