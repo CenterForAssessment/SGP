761fea817781640ad8f46ef651e969f6f6f09816
dbetebenner <dbetebenner@gmail.com>
2015-12-08 21:07:40 -0500

    Update of package to pass CRAN checks and update of date

--- R/getVersion.R
+++ R/getVersion.R
@@ -1,6 +1,6 @@
 `getVersion` <-
-function(data) {
-    if (is.SGP(data) & .hasSlot(data, "Version")) {
+function(data=NULL) {
+    if (is.SGP(data) && .hasSlot(data, "Version")) {
         return(list(SGP_Package_Version=c(data@Version[["SGP_Package_Version"]], as.character(packageVersion("SGP"))),
             Date_Prepared=c(data@Version[["Date_Prepared"]], date())))
     } else {
