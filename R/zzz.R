5c2277fecc4787d10db1c7422cf96823e70927c9
dbetebenner <dbetebenner@gmail.com>
2016-04-05 11:00:00 -0400

    Fixing CI bug in analyzeSGP that didnt pass correct parameter

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (4-4-2016).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (4-5-2016).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki'))
 	}
 }
