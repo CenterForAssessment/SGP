8eaef659b0b9cb36c67d1ec0f0611a9132338aaa
dbetebenner <dbetebenner@gmail.com>
2016-05-08 05:35:23 -0400

    Making getCohortDataInfo data.table compatible

--- R/getCohortDataInfo.R
+++ R/getCohortDataInfo.R
@@ -1,4 +1,4 @@
-`getCohortDataInfo` <- 
+`getCohortDataInfo` <-
 function(sgp.data,
 	par.sgp.config) {
 
@@ -7,7 +7,7 @@ function(sgp.data,
 		tmp.scale_scores.names <- paste("SCALE_SCORE", tail(par.sgp.config[[sgp.iter]][['sgp.panel.years']], 2), tail(par.sgp.config[[sgp.iter]][['sgp.content.areas']], 2), sep=".")
 		tmp.data <- getPanelData(sgp.data, "sgp.percentiles", par.sgp.config[[sgp.iter]])
 		if (all(c(tmp.grades.names, tmp.scale_scores.names) %in% names(tmp.data))) {
-			par.sgp.config[[sgp.iter]][['panel_data_dimension']] <- dim(na.omit(tmp.data[,c("ID", tmp.grades.names, tmp.scale_scores.names)]))
+			par.sgp.config[[sgp.iter]][['panel_data_dimension']] <- dim(na.omit(tmp.data[,c("ID", tmp.grades.names, tmp.scale_scores.names), with=FALSE]))
 		} else {
 			par.sgp.config[[sgp.iter]][['panel_data_dimension']] <- 0
 		}
