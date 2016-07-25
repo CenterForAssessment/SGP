d0e885413ff6d4be44eac5dff2042336e57ae257
dbetebenner <dbetebenner@gmail.com>
2016-07-25 04:00:20 -0400

    Bumping version number with updated cuts for RLI

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (7-22-2016).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (7-25-2016).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki'))
 	}
 }
