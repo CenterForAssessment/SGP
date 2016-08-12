f76af4ddd3cbf4725ee903f070c76248dfb0bc25
dbetebenner <dbetebenner@gmail.com>
2016-08-12 10:17:23 -0400

    Fixing merge conflict

--- R/zzz.R
+++ R/zzz.R
@@ -7,10 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-<<<<<<< HEAD
 		packageStartupMessage(magenta$bold('SGP',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (8-12-2016).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki'))
-=======
-		packageStartupMessage(magenta$bold('SGP',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (8-12-2016).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki'))
->>>>>>> e7ca43e18d48a7f0a7fb849b9ed76a1998c68e7e
 	}
 }
