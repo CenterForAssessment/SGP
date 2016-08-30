7ee82b0bc7753ffd96c4756aa3201f8191ea31f5
dbetebenner <dbetebenner@gmail.com>
2016-08-30 17:05:56 -0400

    Update of studentGrowthPlot to deal better with gap years in Hawaii

--- R/studentGrowthPlot.R
+++ R/studentGrowthPlot.R
@@ -403,9 +403,11 @@ function(Scale_Scores,                        ## Vector of Scale Scores
 	if (!is.null(Report_Parameters[['Assessment_Transition']][['Assessment_Transition_Type']])) {
 		if (identical(toupper(Report_Parameters[['Assessment_Transition']][['Assessment_Transition_Type']][1]), "NO")) {
 			tmp.cutscore.year <- tail(head(sort(unique(Cutscores[['YEAR']]), na.last=FALSE), -1), 1)
-			tmp.cutscore.grade <- Grades[which(Years==Report_Parameters[['Assessment_Transition']][['Year']])]
+			tmp.cutscore.grade <- max(intersect(Grades, grade.values[['interp.df']][['GRADE']][which(grade.values[['years']]<=Report_Parameters[['Assessment_Transition']][['Year']])]))
 			if (is.na(tmp.cutscore.grade)) {
-				tmp.cutscore.grade <- grade.values[['interp.df']][['GRADE']][which(grade.values[['years']]==Report_Parameters[['Assessment_Transition']][['Year']])]
+				tmp.grades.reported <- SGP::SGPstateData[["HI"]][["Student_Report_Information"]][["Grades_Reported"]][[Report_Parameters$Content_Area]]
+				tmp.cutscore.grade <-
+					tmp.grades.reported[which.max(as.numeric(grade.values[['interp.df']][['GRADE']][which(grade.values[['years']]==Report_Parameters[['Assessment_Transition']][['Year']])]) < tmp.grades.reported)-1]
 			}
 			if (is.na(tmp.cutscore.year)) {
 				Cutscores[is.na(YEAR), CUTSCORES:=Cutscores[GRADE==tmp.cutscore.grade & is.na(YEAR)][['CUTSCORES']]]
