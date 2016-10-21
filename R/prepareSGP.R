6f518e13b616fefebfced4d06bd334015db8502d
Adam VanIwaarden <avaniwaarden@gmail.com>
2016-10-21 06:49:27 -0600

    Use canonical key of "data" - can include GRADE if ACHIEVEMENT_LEVEL created.

--- R/prepareSGP.R
+++ R/prepareSGP.R
@@ -265,9 +265,9 @@ function(data,
 		if (identical(toupper(fix.duplicates), "KEEP.ALL")) {
 			if (is.SGP(data)) {
 				assign("DUPLICATED_CASES",
-					data@Data["VALID_CASE"][unique(data@Data["VALID_CASE"][duplicated(data@Data["VALID_CASE"], by=key(data@Data)), c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"), with=FALSE], by=key(data@Data))])
+					data@Data["VALID_CASE"][unique(data@Data["VALID_CASE"][duplicated(data@Data["VALID_CASE"], by=getKey(data@Data)), c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"), with=FALSE], by=getKey(data@Data))])
 			} else {
-				assign("DUPLICATED_CASES", data["VALID_CASE"][unique(data["VALID_CASE"][duplicated(data["VALID_CASE"], by=key(data)), c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"), with=FALSE], by=key(data))])
+				assign("DUPLICATED_CASES", data["VALID_CASE"][unique(data["VALID_CASE"][duplicated(data["VALID_CASE"], by=getKey(data)), c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID"), with=FALSE], by=getKey(data))])
 			}
 			if (dim(DUPLICATED_CASES)[1]!=0) {
 				sgp_object@Data <- createUniqueLongData(sgp_object@Data)
