4d7eed599af2ff1fde30c39c5e5de8f7bb77242e
dbetebenner <dbetebenner@gmail.com>
2017-05-04 06:05:18 -0400

    Fixing loss/hoss bounding for SGPt

--- R/studentGrowthProjections.R
+++ R/studentGrowthProjections.R
@@ -252,7 +252,6 @@ function(panel.data,	## REQUIRED
 									tmp.dt[,TEMP_2:=.smooth.bound.iso.row(
 											data.table(ID=tmp.dt[[1L]], X=TEMP_1),
 											tail(grade.progression, 1L),
-#											grade.projection.sequence[j],
 											yearIncrement(sgp.labels[['my.year']], j, lag.increment),
 											content_area.projection.sequence[j],
 											missing.taus=missing.taus,
