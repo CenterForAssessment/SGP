7024525fa3aa1d71c4ec70dd806fdbc33329eac7
dbetebenner <dbetebenner@gmail.com>
2017-07-15 07:06:04 -0400

    Adding in new cutscores and bumping version number

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <- 
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('SGP ',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (1-24-2017).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki')
+		packageStartupMessage('SGP ',paste(paste(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),' (7-15-2017).  For help: >help("SGP") or visit https://github.com/CenterForAssessment/SGP/wiki')
 	}
 }
