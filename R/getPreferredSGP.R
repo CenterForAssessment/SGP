ea1463ce3320f6014cfeef3a490ce55f697cbbe6
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-08-01 07:46:49 -0600

    Make use of 'sgp.norm.group.preference' element in custom configs backwards compatible with norm group preference object.

--- R/getPreferredSGP.R
+++ R/getPreferredSGP.R
@@ -34,24 +34,38 @@ function(tmp.data,
 		} else tmp.key.vars <- "PREFERENCE"
 
 		if (any(grepl("^PREFERENCE$", names(tmp.data)))) {
-			setkeyv(tmp.data, c(getKey(tmp.data), tmp.key.vars))
-		} else {
+			#  Order data using key
+		  setkeyv(tmp.data, c(getKey(tmp.data), tmp.key.vars))
+		  if (is.null(dup.key)) dup.key <- getKey(tmp.data)
+		  setkeyv(tmp.data, dup.key)
 			if (!is.null(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']])) {
-				messageSGP(paste0(tmp.message, state, "."))
-				data.norm.groups <- unique(as.character(tmp.data[which(duplicated(tmp.data, by=key(tmp.data))), SGP_NORM_GROUP]))
-				metadata.norm.groups <- unique(as.character(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']][[tmp.sgp.norm.group.variables[2]]]))
-				if(!all(data.norm.groups %in% metadata.norm.groups)) {
-					missing.norm.groups <- data.norm.groups[which(!(data.norm.groups %in% metadata.norm.groups))]
-					stop(paste("\n\n\tERROR: Not all", tmp.sgp.norm.group.variables[2], "values present in SGPstateData SGP_Norm_Group_Preference object. Missing Norm Group(s):\n\t\t"), paste(missing.norm.groups, collapse="\n\t\t"))
+				if (any(unique(tmp.data$YEAR) %in% unique(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']]$YEAR))) {
+					new.dups <- tmp.data[!is.na(PREFERENCE),]
+					new.dups <- new.dups[!duplicated(new.dups, by=key(tmp.data))]
+					tmp.data <- rbindlist(list(tmp.data[is.na(PREFERENCE),], new.dups))[, PREFERENCE:=NULL]
+					setkeyv(tmp.data, dup.key)
 				}
-				setkeyv(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']], tmp.sgp.norm.group.variables[1:2])
-				setkeyv(tmp.data, tmp.sgp.norm.group.variables[1:2])
 			} else {
-				stop("\tNOTE: Multiple SGPs exist for individual students. Please examine results in @SGP[['SGPercentiles']].  A SGP_Norm_Group_Preference entry in SGPstateData may be required.")
+			  tmp.data <- tmp.data[!duplicated(tmp.data, by=key(tmp.data))][, PREFERENCE:=NULL]
+		  	if ("SGP_NOTE" %in% names(tmp.data)) invisible(tmp.data[, SGP_NOTE_TF := NULL])
+		  	return(tmp.data)
 			}
-			tmp.data <- data.table(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']][,tmp.sgp.norm.group.variables,with=FALSE][tmp.data],
-				key=c(getKey(tmp.data), tmp.key.vars))
 		}
+		if (!is.null(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']])) {
+			messageSGP(paste0(tmp.message, state, "."))
+			data.norm.groups <- unique(as.character(tmp.data[which(duplicated(tmp.data, by=key(tmp.data))), SGP_NORM_GROUP]))
+			metadata.norm.groups <- unique(as.character(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']][[tmp.sgp.norm.group.variables[2]]]))
+			if(!all(data.norm.groups %in% metadata.norm.groups)) {
+				missing.norm.groups <- data.norm.groups[which(!(data.norm.groups %in% metadata.norm.groups))]
+				stop(paste("\n\n\tERROR: Not all", tmp.sgp.norm.group.variables[2], "values present in SGPstateData SGP_Norm_Group_Preference object. Missing Norm Group(s):\n\t\t"), paste(missing.norm.groups, collapse="\n\t\t"))
+			}
+			setkeyv(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']], tmp.sgp.norm.group.variables[1:2])
+			setkeyv(tmp.data, tmp.sgp.norm.group.variables[1:2])
+		} else {
+			stop("\tNOTE: Multiple SGPs exist for individual students. Please examine results in @SGP[['SGPercentiles']].  A SGP_Norm_Group_Preference entry in SGPstateData may be required.")
+		}
+		tmp.data <- data.table(SGP::SGPstateData[[state]][['SGP_Norm_Group_Preference']][,tmp.sgp.norm.group.variables,with=FALSE][tmp.data],
+			key=c(getKey(tmp.data), tmp.key.vars))
 	}
 
 	if (is.null(dup.key)) dup.key <- getKey(tmp.data)
