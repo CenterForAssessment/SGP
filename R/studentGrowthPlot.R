ac279cae7b10c838b3de96691f7b7151ca953707
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-10-09 06:45:27 -0600

    Label check fix for WIDA CO ISRs

--- R/studentGrowthPlot.R
+++ R/studentGrowthPlot.R
@@ -393,9 +393,11 @@ function(Scale_Scores,                        ## Vector of Scale Scores
 			label.split <- unlist(strsplit(label, " "))
 			label.word.nchar <- cumsum(nchar(label.split))
 			if (length(label.word.nchar) > 1 & tail(label.word.nchar, 1) > 10) {
-				label.split.position <- max(which(label.word.nchar <= 10))
-				tmp.label <- paste(label.split[1:label.split.position], collapse=" ")
-				tmp.label[2] <- paste(label.split[(label.split.position+1):length(label.split)], collapse=" ")
+				if (any(label.word.nchar <= 10)) {
+					label.split.position <- max(which(label.word.nchar <= 10))
+					tmp.label <- paste(label.split[1:label.split.position], collapse=" ")
+					tmp.label[2] <- paste(label.split[(label.split.position+1):length(label.split)], collapse=" ")
+				} else tmp.label <- label.split
 				tmp.cex <- c(title.ca.size - 0.25 - max(0, nchar(tmp.label[1])-10)*0.1, title.ca.size - 0.25 - max(0, nchar(tmp.label[2])-10)*0.1)
 				return(list(content_area.label.pieces=tmp.label, cex=tmp.cex))
 			} else {
