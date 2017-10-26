059a2c1d86e5cd4c2d43b5029f601eb759e7170f
Adam VanIwaarden <avaniwaarden@gmail.com>
2017-10-25 21:49:39 -0600

    Updates to metadata for Georgia projections

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (10-15-2017). For help: >help("SGP") or visit https://sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (10-25-2017). For help: >help("SGP") or visit https://sgp.io'))
 	}
 }
