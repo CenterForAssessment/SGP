7109821637af94c3d58a03d8769e270e77ecd11d
dbetebenner <dbetebenner@gmail.com>
2017-11-06 12:20:54 -0500

    Update of meta-data for New Hampshire

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (11-1-2017). For help: >help("SGP") or visit https://sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (11-6-2017). For help: >help("SGP") or visit https://sgp.io'))
 	}
 }
