32b4eb44213d2afcc560a8322067afcfe8235e06
dbetebenner <dbetebenner@gmail.com>
2017-11-14 16:47:57 -0500

    Update of combineSGP

--- R/combineSGP.R
+++ R/combineSGP.R
@@ -475,7 +475,7 @@ function(
 
 			for (i in seq_along(target.args[['my.sgp']])) {
 				for (target.years.iter in max.sgp.target.years.forward) {
-					if (!grepl("BASELINE", target.args[['my.sgp']][i])) my.label <- paste("CATCH_UP_KEEP_UP_STATUS", target.years.iter, sep="_") else my.label <- paste("CATCH_UP_KEEP_UP_STATUS_BASELINE", target.years.iter, sep="_")
+					if (!grepl("BASELINE", target.args[['my.sgp']][i])) my.label <- paste("CATCH_UP_KEEP_UP_STATUS", target.years.iter, "YEAR", sep="_") else my.label <- paste("CATCH_UP_KEEP_UP_STATUS_BASELINE", target.years.iter, "YEAR", sep="_")
 					if (grepl("FROM", target.args[['my.sgp']][i])) slot.data[YEAR <= year.for.equate, target.args[['my.sgp']][i]:=SGP] ### Get comparison values from before transition
 					if (my.label %in% names(slot.data)) slot.data[,(my.label):=NULL]
 					slot.data[,(my.label):=rep(as.character(NA), dim(slot.data)[1])]
@@ -519,7 +519,7 @@ function(
 
 			for (i in seq_along(target.args[['my.sgp']])) {
 				for (target.years.iter in max.sgp.target.years.forward) {
-					if (!grepl("BASELINE", target.args[['my.sgp']][i])) my.label <- paste("MOVE_UP_STAY_UP_STATUS", target.years.iter, sep="_") else my.label <- paste("MOVE_UP_STAY_UP_STATUS_BASELINE", target.years.iter, sep="_")
+					if (!grepl("BASELINE", target.args[['my.sgp']][i])) my.label <- paste("MOVE_UP_STAY_UP_STATUS", target.years.iter, "YEAR", sep="_") else my.label <- paste("MOVE_UP_STAY_UP_STATUS_BASELINE", target.years.iter, "YEAR", sep="_")
 					if (grepl("FROM", target.args[['my.sgp']][i])) slot.data[YEAR <= year.for.equate, target.args[['my.sgp']][i]:=SGP]
 					if (my.label %in% names(slot.data)) slot.data[,(my.label):=NULL]
 					slot.data[,(my.label):=rep(as.character(NA), dim(slot.data)[1])]
