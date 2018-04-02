ad6db75fb6a3cc3e5fcd4061f33f5ba12757bc47
dbetebenner <dbetebenner@gmail.com>
2018-04-02 14:23:25 -0400

    Adding in merges

--- R/zzz.R
+++ R/zzz.R
@@ -7,10 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-<<<<<<< HEAD
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (2-15-2018). For help: >help("SGP") or visit https://sgp.io'))
-=======
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (3-31-2018). For help: >help("SGP") or visit https://sgp.io'))
->>>>>>> dcc12574f9e284cb86bddb2e4f59e759135644cc
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (4-2-2018). For help: >help("SGP") or visit https://sgp.io'))
 	}
 }
