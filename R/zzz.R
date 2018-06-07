bfe2a37c416931dae68516dd1049087c8502bf67
dbetebenner <dbetebenner@gmail.com>
2018-06-07 16:47:46 -0400

    Fixing errant git conflict

--- R/zzz.R
+++ R/zzz.R
@@ -7,10 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-<<<<<<< HEAD
 		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-6-2018). For help: >help("SGP") or visit sgp.io'))
-=======
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-5-2018). For help: >help("SGP") or visit https://sgp.io'))
->>>>>>> d1f7cb00f36a0fcc4df0e5c7a97e4ff847f1f1c1
 	}
 }
