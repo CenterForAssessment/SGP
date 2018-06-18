84244ec7c56615127271e22114953384a29e8383
Damian Betebenner <dbetebenner@nciea.org>
2018-06-18 11:04:51 -0400

    Externalizing getScaleScoreTarget

--- R/combineSGP.R
+++ R/combineSGP.R
@@ -611,49 +611,7 @@ function(
 			}
 		}
 		if (!identical(sgp.target.scale.scores.merge, FALSE)) {
-			tmp.list <- list()
-			if (identical(sgp.target.scale.scores.merge, "1_year_lagged")) {
-				tmp.names <- grep(paste(years, "LAGGED.TARGET_SCALE_SCORES", sep="."), names(sgp_object@SGP$SGProjections), value=TRUE)
-				for (i in tmp.names) {
-					tmp.list[[i]] <- data.table(
-						VALID_CASE="VALID_CASE",
-						CONTENT_AREA=unlist(strsplit(i, "[.]"))[1],
-						YEAR=getTableNameYear(i),
-						sgp_object@SGP[["SGProjections"]][[i]])
-				}
-				tmp.dt <- rbindlist(tmp.list, fill=TRUE)
-				tmp.cols <- grep("YEAR_1", names(tmp.dt), value=TRUE)
-				slot.data[tmp.dt, (tmp.cols):=mget(tmp.cols), on=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "GRADE")]
-			}
-			if (identical(sgp.target.scale.scores.merge, "1_year_lagged_current")) {
-				tmp.names <- c(grep(paste(years, "LAGGED.TARGET_SCALE_SCORES", sep="."), names(sgp_object@SGP$SGProjections), value=TRUE),
-					grep(paste(years, "TARGET_SCALE_SCORES", sep="."), names(sgp_object@SGP$SGProjections), value=TRUE))
-				for (i in tmp.names) {
-					tmp.list[[i]] <- data.table(
-						VALID_CASE="VALID_CASE",
-						CONTENT_AREA=unlist(strsplit(i, "[.]"))[1],
-						YEAR=getTableNameYear(i),
-						sgp_object@SGP[["SGProjections"]][[i]])
-				}
-				tmp.dt <- rbindlist(tmp.list, fill=TRUE)
-				tmp.cols <- grep("YEAR_1", names(tmp.dt), value=TRUE)
-				slot.data[tmp.dt, (tmp.cols):=mget(tmp.cols), on=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "GRADE")]
-			}
-			if (identical(sgp.target.scale.scores.merge, "all_years_lagged_current")) {
-				tmp.names <- c(grep(paste(years, "LAGGED.TARGET_SCALE_SCORES", sep="."), names(sgp_object@SGP$SGProjections), value=TRUE),
-					grep(paste(years, "TARGET_SCALE_SCORES", sep="."), names(sgp_object@SGP$SGProjections), value=TRUE))
-				for (i in tmp.names) {
-					tmp.list[[i]] <- data.table(
-						VALID_CASE="VALID_CASE",
-						CONTENT_AREA=unlist(strsplit(i, "[.]"))[1],
-						YEAR=getTableNameYear(i),
-						sgp_object@SGP[["SGProjections"]][[i]])
-				}
-				tmp.dt <- rbindlist(tmp.list, fill=TRUE)
-				tmp.cols <- setdiff(names(tmp.dt), c("ID", "GRADE", "SGP_PROJECTION_GROUP", "SGP_PROJECTION_GROUP_SCALE_SCORES"))
-				slot.data[tmp.dt, (tmp.cols):=mget(tmp.cols), on=c("VALID_CASE", "CONTENT_AREA", "YEAR", "ID", "GRADE")]
-			}
-
+			slot.data <- mergeScaleScoreTarget(sgp_object, slot.data, years, sgp.target.scale.scores.merge) 
 		}
 	} ### END if (sgp.target.scale.scores)
 
