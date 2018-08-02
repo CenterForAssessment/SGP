faf72d5e49488b4ada214a0c9dedb551928bacf1
Damian Betebenner <dbetebenner@nciea.org>
2018-08-02 12:34:20 -0400

    Fixing up detectSGPCores for large instances

--- R/detectSGPCores.R
+++ R/detectSGPCores.R
@@ -1,4 +1,4 @@
 `detectSGPCores` <-
 function(logical=TRUE) {
-    return(min(detectCores(logical=logical), 100L))
+    return(min(detectCores(logical=logical), 50L))
 } ### END detectSGPCores Function
