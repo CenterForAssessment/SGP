891e776aa1e7c93c7d22c2d5ef7e4ac7368e8a0e
Adam VanIwaarden <avaniwaarden@gmail.com>
2019-06-21 08:29:28 -0600

    Add DoDEA SGPstateData (temporary)

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-18-2019). For help: >help("SGP") or visit sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (6-21-2019). For help: >help("SGP") or visit sgp.io'))
 	}
 }
