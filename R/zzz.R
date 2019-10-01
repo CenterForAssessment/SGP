7e75de35254b415e0c562dc57209acfe03422194
Damian Betebenner <dbetebenner@nciea.org>
2019-10-01 09:32:28 -0400

    Bumping number to 1.9-4.0

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (9-30-2019). For help: >help("SGP") or visit sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (10-1-2019). For help: >help("SGP") or visit sgp.io'))
 	}
 }
