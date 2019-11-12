e40b2a29103af7255f09ad5c69a88fe474953ba8
Damian Betebenner <dbetebenner@nciea.org>
2019-11-12 05:25:44 -0500

    Update of MI meta-data

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (11-11-2019). For help: >help("SGP") or visit sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (11-12-2019). For help: >help("SGP") or visit sgp.io'))
 	}
 }
