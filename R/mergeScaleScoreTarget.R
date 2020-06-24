b43c732d12cd84b9eb784d52578c04b6a051912b
Damian Betebenner <dbetebenner@nciea.org>
2020-06-23 21:44:17 -0400

    Update of mergeScaleScoreTarget

--- R/mergeScaleScoreTarget.R
+++ R/mergeScaleScoreTarget.R
@@ -15,6 +15,9 @@ function(sgp_object,
 					CONTENT_AREA=unlist(strsplit(i, "[.]"))[1L],
 					YEAR=getTableNameYear(i),
 					sgp_object@SGP[["SGProjections"]][[i]])
+				if (any(duplicated(tmp.list[[i]], by=getKey(tmp.list[[i]])))) {
+					tmp.list[[i]] <- getPreferredSGP(tmp.list[[i]], state=state, type="TARGET")
+				}
 			}
 			tmp.dt <- rbindlist(tmp.list, fill=TRUE)
 			tmp.index <- slot.data[tmp.dt[, getKey(slot.data), with=FALSE], which=TRUE, on=getKey(slot.data)]
@@ -52,6 +55,9 @@ function(sgp_object,
 					CONTENT_AREA=unlist(strsplit(i, "[.]"))[1L],
 					YEAR=getTableNameYear(i),
 					sgp_object@SGP[["SGProjections"]][[i]])
+				if (any(duplicated(tmp.list[[i]], by=getKey(tmp.list[[i]])))) {
+					tmp.list[[i]] <- getPreferredSGP(tmp.list[[i]], state=state, type="TARGET")
+				}
 			}
 			tmp.dt <- rbindlist(tmp.list[grep("LAGGED", tmp.names)], fill=TRUE)
 			tmp.index <- slot.data[tmp.dt[, getKey(slot.data), with=FALSE], which=TRUE, on=getKey(slot.data)]
