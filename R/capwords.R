a990ca86c6f72c85fd96c19b76a1c80cf65938b4
Damian Betebenner <dbetebenner@nciea.org>
2021-11-12 14:13:39 -0500

    Fixing up an error checking warning in studentGrowthPercentiles

--- R/capwords.R
+++ R/capwords.R
@@ -1,6 +1,6 @@
 `capwords` <-
 function(x,
-	special.words = c("ELA","I", "II", "III", "IV", "EMH", "HS", "MS", "ES", "SES", "IEP", "ELL", "MAD", "PARCC", "SBAC", "SD", "SWD", "US", "SGP", "SIMEX", "SS", "SAT", "PSAT")) {
+	special.words = c("ELA","I", "II", "III", "IV", "DCYF", "EMH", "HS", "MS", "ES", "SES", "IEP", "ELL", "MAD", "PARCC", "SBAC", "SD", "SWD", "US", "SGP", "SIMEX", "SS", "SAT", "PSAT")) {
 
 	if (is.null(x)) return(NULL)
 	if (is.na(x)) return(NA)
