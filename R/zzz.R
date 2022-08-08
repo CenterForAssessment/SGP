2fe724ad997a7afe140343a87f18aa3565cae3d2
Adam VanIwaarden <avaniwaarden@gmail.com>
2022-08-08 11:16:29 -0600

    Update SGPstateData for UT

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (8-1-2022). For help: >help("SGP") or visit sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (8-8-2022). For help: >help("SGP") or visit sgp.io'))
 	}
 }
