cf4e83f3f5f75e54bf92516f1240cd8af1b4de95
Damian Betebenner <dbetebenner@nciea.org>
2023-01-08 19:56:43 -0500

    Adding doRNG and updating ERB data

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (12-7-2022). For help: >help("SGP") or visit sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (1-8-2023). For help: >help("SGP") or visit sgp.io'))
 	}
 }
