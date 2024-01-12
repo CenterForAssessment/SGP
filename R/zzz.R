6f50dc27848cdb9aff607f3b4d6cfa0d8e9b48c1
Damian Betebenner <dbetebenner@nciea.org>
2024-01-12 10:31:36 -0500

    Updating WIDA_RI meta-data

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (10-23-2023). For help: >help("SGP") or visit sgp.io'))
+		packageStartupMessage(magenta$bold('SGP',paste(paste0(unlist(strsplit(as.character(packageVersion("SGP")), "[.]")), c(".", "-", ".", "")), collapse=""),' (1-12-2024). For help: >help("SGP") or visit sgp.io'))
 	}
 }
