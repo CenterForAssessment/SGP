1ad96926600d28274d7023d43ed207774174cc38
Damian Betebenner <dbetebenner@nciea.org>
2024-12-04 17:14:47 -1000

    Fixing up the start up message

--- R/zzz.R
+++ R/zzz.R
@@ -5,16 +5,30 @@ function(libname, pkgname) {
 	utils::globalVariables(c("."))
 }
 
-.onAttach <- function(libname, pkgname) {
+`.onAttach` <- function(libname, pkgname) {
     if (interactive()) {
+
+        get_dev_version <- function(package) {
+            url <- paste0("https://raw.githubusercontent.com/CenterForAssessment/", package, "/refs/heads/master/DESCRIPTION")
+            lines <- readLines(url, warn = FALSE)
+            return(strsplit(grep("^Version:", lines, value = TRUE), ": ")[[1]][2])
+        }
+
         # Extract version information
-        version <- utils::packageVersion("SGP")
+        installed.version <- utils::packageDescription("SGP")[['Version']]
+        cran.version <- pkgsearch::cran_package("SGP")[['Version']]
+        dev.version <- get_dev_version("SGP")
 
         # Define a friendly startup message
 	message_text <- paste0(
-	    magenta(bold("\uD83C\uDF89 SGP v", version)), " - ", toOrdinal::toOrdinalDate("2024-12-3"), "\n",
-	    "\U1F4A1 Tip: ", magenta(bold("> help(\"SGP\")")), "\n",
+	    magenta(bold("\uD83C\uDF89 SGP v", installed.version, sep="")), " - ", toOrdinal::toOrdinalDate("2024-12-4"), "\n",
+		strrep("━", 40), "\n",
+        bold("\U1F4E6 CRAN Version: "), green(cran.version), "\n",
+        bold("\U1F527 Dev Version: "), cyan(dev.version), "\n",
+		strrep("━", 40), "\n",
+	    "\U1F4A1 Tip: ", magenta(bold("> help(package=\"SGP\")")), "\n",
 	    "\U1F310 Docs: ", magenta(bold("https://sgp.io/")), "\n",
+		strrep("━", 40), "\n",
 	    "\u2728 Happy SGPing!"
 	)
 
